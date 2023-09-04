7a3b9230d4e274cb1a6edab7f77500e7745a125d
championliu <championliu@tencent.com>
2023-09-04 10:49:53 +0800

    update test_log.rs and .gitignore

--- tests/test_log.rs
+++ tests/test_log.rs
@@ -22,7 +22,7 @@ fn test_log_levels(){
 
 fn test_log_write_file() {
 
-    log4rs::init_file("log4rs.yml", Default::default()).unwrap();
+    log4rs::init_file("config/log4rs.yml", Default::default()).unwrap();
 
     /* 
 
