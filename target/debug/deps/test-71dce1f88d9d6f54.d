b2f21e9cb136cfecf7c7a085d54620cfebcaf405
loboxu <loboxu@tencent.com>
2023-10-13 10:12:57 +0800

    init group

--- target/debug/deps/test-71dce1f88d9d6f54.d
+++
@@ -1,5 +0,0 @@
-/Users/championliu/Documents/projects/rust/robustmq/target/debug/deps/test-71dce1f88d9d6f54.rmeta: tests/test.rs
-
-/Users/championliu/Documents/projects/rust/robustmq/target/debug/deps/test-71dce1f88d9d6f54.d: tests/test.rs
-
-tests/test.rs:
--- target/debug/deps/test-76be189291ed19cc.d
+++
@@ -1,5 +0,0 @@
-/Users/championliu/Documents/projects/rust/robustmq/target/debug/deps/test-76be189291ed19cc.rmeta: benches/test.rs
-
-/Users/championliu/Documents/projects/rust/robustmq/target/debug/deps/test-76be189291ed19cc.d: benches/test.rs
-
-benches/test.rs:
