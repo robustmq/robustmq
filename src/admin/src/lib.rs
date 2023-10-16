d6cab69965caf27251d422cbe78ce47d335478c6
loboxu <loboxu@tencent.com>
2023-10-16 22:15:41 +0800

    meta module init

--- src/admin/src/lib.rs
+++ src/admin/src/lib.rs
@@ -1,4 +1,4 @@
-use std::{net::SocketAddr, str::FromStr};
+use std::{net::SocketAddr, str::FromStr, thread::{self, JoinHandle}};
 
 use axum::{routing::get, Router};
 
@@ -8,25 +8,29 @@ const ROUTE_METRICS: &str = "/metrics";
 mod prometheus;
 mod welcome;
 
-pub fn start(addr: &String, port: Option<u16>, worker_threads: usize) {
+pub fn start(addr: String, port: Option<u16>, worker_threads: usize) -> JoinHandle<()>{
+    let handle = thread::spawn(move || {
+        let runtime = tokio::runtime::Builder::new_current_thread()
+            .worker_threads(worker_threads)
+            .thread_name("admin-http")
+            .enable_io()
+            .build()
+            .unwrap();
 
-    let runtime = tokio::runtime::Builder::new_current_thread()
-        .worker_threads(worker_threads)
-        .thread_name("admin-http")
-        .enable_io()
-        .build()
-        .unwrap();
-
-    runtime.block_on(async {
-        let app = Router::new()
+        runtime.block_on(async {
+            let app = Router::new()
                 .route(ROUTE_METRICS, get(prometheus::handler))
                 .route(ROUTE_ROOT, get(welcome::handler));
 
-        let ip = SocketAddr::from_str(&format!("{}:{}",addr,port.unwrap())).unwrap();
-        axum::Server::bind(&ip).serve(app.into_make_service()).await.unwrap();
-    })
-} 
-
+            let ip = SocketAddr::from_str(&format!("{}:{}", addr, port.unwrap())).unwrap();
+            axum::Server::bind(&ip)
+                .serve(app.into_make_service())
+                .await
+                .unwrap();
+        })
+    });
+   return handle;
+}
 
 #[cfg(test)]
 mod tests {
