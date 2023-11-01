d139aa341e7231e2f19e5fb6048702d5bf762154
loboxu <loboxu@tencent.com>
2023-11-01 17:14:25 +0800

    raft dev

--- src/meta/storage/rdb_storage.rs
+++ src/meta/storage/rdb_storage.rs
@@ -1,4 +1,5 @@
 use raft::Storage as RaftStorage;
+use raft::Result as RaftResult;
 
 struct RaftRocksDBStorage{
 
@@ -13,10 +14,35 @@ impl RaftRocksDBStorage{
 }
 
 impl RaftStorage for RaftRocksDBStorage{
-    fn initial_state(&self) -> raft::Result<raft::RaftState> {
+    fn initial_state(&self) -> RaftResult<raft::RaftState> {
         
     }
 
+    fn entries(
+            &self,
+            low: u64,
+            high: u64,
+            max_size: impl Into<Option<u64>>,
+            context: raft::GetEntriesContext,
+        ) -> RaftResult<Vec<Entry>> {
+        
+    }
+
+    fn first_index(&self) -> RaftResult<u64> {
+        
+    }
+
+    fn last_index(&self) -> RaftResult<u64> {
+        
+    }
+
+    fn snapshot(&self, request_index: u64, to: u64) -> RaftResult<Snapshot> {
+        
+    }
+
+    fn term(&self, idx: u64) -> RaftResult<u64> {
+        
+    }
 }
 
 #[cfg(test)]
