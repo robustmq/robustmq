7a71f8d5d568429a870f8b52be69f1e4e4a1bba8
loboxu.1227 <loboxu.1227@bytedance.com>
2023-11-21 15:11:38 +0800

    Directory structure is introduced into workspace

--- src/common/src/lib.rs
+++ src/common/src/lib.rs
@@ -12,28 +12,4 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-pub const DEFAULT_LOG_CONFIG: &str = "config/log4rs.yml";
-
-pub fn info(msg: &str) -> () {
-    log::info!(target:"app::server", "{}",msg)
-}
-
-pub fn server_info(msg: &str) -> () {
-    log::info!(target:"app::server", "{}",msg)
-}
-
-pub fn new(){
-    log4rs::init_file(DEFAULT_LOG_CONFIG, Default::default()).unwrap();
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn log_print() {
-        log4rs::init_file(format!("../../{}", DEFAULT_LOG_CONFIG), Default::default()).unwrap();
-        info("lobo");
-        server_info("server lobo");
-    }
-}
+mod log;
