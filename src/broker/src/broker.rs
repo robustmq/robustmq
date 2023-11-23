cae93b3af1f8683a8289c3937cda2f909582b36c
loboxu.1227 <loboxu.1227@bytedance.com>
2023-11-23 20:07:01 +0800

    broker network dev

---
+++ src/broker/src/broker.rs
@@ -0,0 +1,21 @@
+use std::{net::SocketAddr, fmt::Result};
+
+use tokio::{net::TcpListener, time::error::Elapsed, io};
+
+#[derive(Debug, thiserror::Error)]
+#[error("Acceptor error")]
+pub enum Error {
+    #[error("I/O {0}")]
+    Io(#[from] io::Error),
+    #[error("Timeout")]
+    Timeout(#[from] Elapsed),
+}
+
+pub struct Broker {}
+
+impl Broker {
+    async fn start() -> Result<(),Error>{
+        let ip: SocketAddr = "".parse().unwrap();
+        let listener = TcpListener::bind(&ip).await?;
+    }
+}
--- src/broker/src/lib.rs
+++ src/broker/src/lib.rs
@@ -11,3 +11,12 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+mod broker;
+
+#[cfg(test)]
+mod tests{
+    #[test]
+    fn start_broker(){
+
+    }
+}
