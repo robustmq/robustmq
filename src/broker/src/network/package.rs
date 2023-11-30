ccdbb12664c51996fc75e93dd9f8a7be9c2e5fe7
loboxu.1227 <loboxu.1227@bytedance.com>
2023-11-30 18:02:39 +0800

    network dev

--- src/broker/src/network/package.rs
+++ src/broker/src/network/package.rs
@@ -1,24 +1,31 @@
-use super::connection::Connection;
-
 #[derive(Debug)]
 pub struct RequestPackage {
+    pub content_size: usize,
     pub content: String,
-    pub conn:Connection,
+    pub connection_id: u64,
 }
 
 impl RequestPackage {
-    pub fn new(content: String,conn: Connection) -> Self {
-        Self { content ,conn}
+    pub fn new(content_size: usize, content: String, connection_id: u64) -> Self {
+        Self {
+            content_size,
+            content,
+            connection_id,
+        }
     }
 }
 
 #[derive(Debug)]
 pub struct ResponsePackage {
     pub content: String,
+    pub connection_id: u64,
 }
 
 impl ResponsePackage {
-    pub fn new(content: String) -> Self {
-        Self { content }
+    pub fn new(content: String, connection_id: u64) -> Self {
+        Self {
+            content,
+            connection_id,
+        }
     }
 }
--- src/broker/src/network/tcp_server.rs
+++ src/broker/src/network/tcp_server.rs
@@ -1,21 +1,18 @@
-use crate::network::package::RequestPackage;
-
 use super::{
     connection::{Connection, ConnectionManager},
     package::ResponsePackage,
 };
-use bytes::Bytes;
+use crate::network::package::RequestPackage;
+use bytes::{BytesMut, BufMut};
 use common_log::log::{error, info};
 use flume::{Receiver, Sender};
-use futures::{SinkExt, StreamExt};
 use std::{fmt::Error, net::SocketAddr, sync::Arc};
-use tokio::{net::TcpListener, sync::RwLock};
-use tokio_util::codec::{Framed, LengthDelimitedCodec};
+use tokio::{io::{AsyncReadExt, AsyncWriteExt}, net::TcpListener, sync::RwLock};
 
 pub struct TcpServer {
     ip: SocketAddr,
+    connection_manager: Arc<RwLock<ConnectionManager>>,
     accept_thread_num: usize,
-    max_connection_num: usize,
     handler_process_num: usize,
     response_process_num: usize,
     request_queue_sx: Sender<RequestPackage>,
@@ -39,10 +36,12 @@ impl TcpServer {
         let (response_queue_sx, response_queue_rx) =
             flume::bounded::<ResponsePackage>(response_queue_size);
 
+        let connection_manager: Arc<RwLock<ConnectionManager>> =
+            Arc::new(RwLock::new(ConnectionManager::new(max_connection_num)));
         Self {
             ip,
+            connection_manager,
             accept_thread_num,
-            max_connection_num,
             handler_process_num,
             response_process_num,
             request_queue_sx,
@@ -53,14 +52,14 @@ impl TcpServer {
     }
 
     pub async fn start(&self) {
-        let connection_manager: Arc<RwLock<ConnectionManager>> =
-            Arc::new(RwLock::new(ConnectionManager::new(self.max_connection_num)));
 
         let listener = TcpListener::bind(self.ip).await.unwrap();
         let arc_listener = Arc::new(listener);
-        
+
         for _ in 0..=self.accept_thread_num {
-            _ = self.acceptor(connection_manager.clone(),arc_listener.clone()).await;
+            _ = self
+                .acceptor(arc_listener.clone())
+                .await;
         }
 
         for _ in 0..=self.handler_process_num {
@@ -77,35 +76,46 @@ impl TcpServer {
 
     async fn acceptor(
         &self,
-        connection_manager: Arc<RwLock<ConnectionManager>>,
         listener: Arc<TcpListener>,
     ) -> Result<(), Error> {
         let request_queue_sx = self.request_queue_sx.clone();
+        let connection_manager = self.connection_manager.clone();
+
         tokio::spawn(async move {
             let (stream, addr) = listener.accept().await.unwrap();
+            let socket = Arc::new(RwLock::new(Box::new(stream)));
+            // tls check
 
+            // user login check
+
+            // manager connection info
+            let conn = Connection::new(addr, socket.clone());
+            let connection_id = conn.connection_id();
+            let mut cm = connection_manager.write().await;
+            _ = cm.add(conn);
+
+            // request is processed by a separate thread, placing the request packet in the request queue.\
             tokio::spawn(async move {
-                // todo Frames Codes need to be adjusted
-                let mut stream = Framed::new(stream, LengthDelimitedCodec::new());
-                // let socket = Arc::new(RwLock::new(stream));
-                // let r = socket.clone();
-                let mut socket: Box<Framed<tokio::net::TcpStream, LengthDelimitedCodec>> = Box::new(stream);
-                let conn = Connection::new(addr,socket);
-                let mut cm = connection_manager.write().await;
-                _ = cm.add(conn.clone());
-                // todo
-                while let Some(Ok(data)) = socket.next().await {
-                    let content = String::from_utf8_lossy(&data).to_string();
-                    let package = RequestPackage::new(content, conn);
-                    match request_queue_sx.send(package) {
-                        Ok(_) => {}
-                        Err(err) => error(&format!(
-                            "Failed to write data to the request queue, error message: {:?}",
-                            err
-                        )),
-                    }
-                }
+                let mut read_buf = BytesMut::with_capacity(20);
+                let mut r1 = socket.write().await;
 
+                match r1.read_buf(&mut read_buf).await {
+                    Ok(size) => {
+                        let content = String::from_utf8_lossy(&read_buf).to_string();
+                        let package = RequestPackage::new(size, content, connection_id);
+                        match request_queue_sx.send(package) {
+                            Ok(_) => {}
+                            Err(err) => error(&format!(
+                                "Failed to write data to the request queue, error message: {:?}",
+                                err
+                            )),
+                        }
+                    }
+                    Err(err) => error(&format!(
+                        "Failed to read data from TCP connection, error message: {:?}",
+                        err
+                    )),
+                }
             });
         });
 
@@ -117,8 +127,11 @@ impl TcpServer {
         let response_queue_sx = self.response_queue_sx.clone();
         tokio::spawn(async move {
             while let Ok(resquest_package) = request_queue_rx.recv() {
+                // todo Business logic processing
+
+                // Writes the result of the business logic processing to the return queue
                 let resp = format!("robustmq response:{:?}", resquest_package);
-                let response_package = ResponsePackage::new(resp);
+                let response_package = ResponsePackage::new(resp, resquest_package.connection_id);
                 match response_queue_sx.send(response_package) {
                     Ok(_) => {}
                     Err(err) => error(&format!(
@@ -133,8 +146,26 @@ impl TcpServer {
 
     async fn response_process(&self) -> Result<(), Error> {
         let response_queue_rx = self.response_queue_rx.clone();
+        let connect_manager = self.connection_manager.clone();
         tokio::spawn(async move {
             while let Ok(response_package) = response_queue_rx.recv() {
+                // Logical processing of data response
+                
+                // Write the data back to the client
+                let cm = connect_manager.write().await;
+                let connection = cm.get(response_package.connection_id).unwrap();
+                let mut stream =connection.socket.write().await;
+
+                let mut write_buf = BytesMut::with_capacity(20);
+                write_buf.put(&b"loboxu nb"[..]);
+                match stream.write_all(&write_buf).await{
+                    Ok(_) => {},
+                    Err(err) => error(&format!(
+                        "Failed to write data to the response queue, error message ff: {:?}",
+                        err
+                    )),
+                }
+                
                 println!("{:?}", response_package);
             }
         });
@@ -150,6 +181,6 @@ mod test {
     #[test]
     fn network_server_test() {
         let ip: SocketAddr = "127.0.0.1:8768".parse().unwrap();
-        let net_s = TcpServer::new(ip, 10, 10, 1, 1, 1, 1);
+        // let net_s = TcpServer::new(ip, 10, 10, 1, 1, 1, 1);
     }
 }
