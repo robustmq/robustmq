a0e2ed051caa9ae7edc69c1513a90173fa367f12
loboxu.1227 <loboxu.1227@bytedance.com>
2024-01-26 17:15:40 +0800

    raft dev

--- src/meta/src/http/cluster.rs
+++ src/meta/src/http/cluster.rs
@@ -14,16 +14,40 @@
  * limitations under the License.
  */
 
-use super::response::{success_response, IndexResponse};
-use crate::cluster::Cluster;
+use super::response::{success_response, IndexResponse, RaftInfo};
+use crate::{cluster::Cluster, storage::raft_core::RaftRocksDBStorageCore};
 use std::sync::{Arc, RwLock};
 
-pub async fn controller_index(cluster: Arc<RwLock<Cluster>>) -> String {
-    let cs = cluster.read().unwrap();
+pub async fn controller_index(
+    cluster: Arc<RwLock<Cluster>>,
+    storage: Arc<RwLock<RaftRocksDBStorageCore>>,
+) -> String {
+    let cluster_read = cluster.read().unwrap();
+    let storage = storage.read().unwrap();
+
+    let hs = storage.hard_state();
+    let cs = storage.conf_state();
+    let uncommit_index = storage.uncommit_index();
+
+    let raft_info = RaftInfo {
+        role: format!("{:?}", cluster_read.raft_role),
+        first_index: storage.first_index(),
+        last_index: storage.last_index(),
+        term: hs.term,
+        vote: hs.vote,
+        commit: hs.commit,
+        voters: cs.voters.to_vec(),
+        learners: cs.learners.to_vec(),
+        voters_outgoing: cs.voters_outgoing.to_vec(),
+        learners_next: cs.learners_next.to_vec(),
+        auto_leave: cs.auto_leave,
+        uncommit_index: uncommit_index,
+    };
+
     let resp = IndexResponse {
-        local: cs.local.clone(),
-        node_lists: cs.peers.clone(),
-        role: format!("{:?}", cs.raft_role),
+        local: cluster_read.local.clone(),
+        node_lists: cluster_read.peers.clone(),
+        raft: raft_info,
     };
 
     return success_response(resp);
