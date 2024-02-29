ec03cef7dec2e48bc5362795f19e69fc2858761a
loboxu <loboxu@tencent.com>
2024-02-29 10:02:02 +0800

    dev

--- src/clients/src/lib.rs
+++ src/clients/src/lib.rs
@@ -1,3 +1,24 @@
+use std::collections::HashMap;
+use lazy_static::lazy_static;
+use placement_center::build_placement_center_client;
+use protocol::placement_center::placement::placement_center_service_client::PlacementCenterServiceClient;
+use tonic::transport::Channel;
+
 pub mod broker_server;
 pub mod placement_center;
 pub mod storage_engine;
+
+lazy_static! {
+    static ref PLACEMENT_CENTER_CLUSTER_CLIENTS: HashMap<String, PlacementCenterServiceClient<Channel>> =
+        HashMap::new();
+}
+
+pub async fn placement_center_client<'a>(addr: String) -> Option<&'a PlacementCenterServiceClient<Channel>>{
+    if PLACEMENT_CENTER_CLUSTER_CLIENTS.contains_key(&addr) {
+        if let Some(client) = PLACEMENT_CENTER_CLUSTER_CLIENTS.get(&addr) {
+            return Some(client);
+        }
+    }
+    let client = build_placement_center_client(&addr).await;
+    return None;
+}
--- src/clients/src/placement_center/mod.rs
+++ src/clients/src/placement_center/mod.rs
@@ -14,76 +14,88 @@
  * limitations under the License.
  */
 
- use common::errors::RobustMQError;
- use protocol::placement_center::placement::{ placement_center_service_client::PlacementCenterServiceClient, CommonReply, CreateShardRequest, DeleteShardRequest, HeartbeatRequest, RegisterNodeRequest, SendRaftConfChangeReply, SendRaftConfChangeRequest, SendRaftMessageReply, SendRaftMessageRequest, UnRegisterNodeRequest
- };
- 
- 
- pub async fn register_node(
-     addr: &String,
-     request: RegisterNodeRequest,
- ) -> Result<CommonReply, RobustMQError> {
-     let mut client = match PlacementCenterServiceClient::connect(format!("http://{}", addr)).await {
-         Ok(client) => client,
-         Err(err) => return Err(RobustMQError::TonicTransport(err)),
-     };
- 
-     let resp = match client.register_node(tonic::Request::new(request)).await {
-         Ok(reply) => reply.into_inner(),
-         Err(status) => return Err(RobustMQError::MetaGrpcStatus(status)),
-     };
-     return Ok(resp);
- }
- 
- pub async fn unregister_node(
-     addr: &String,
-     request: UnRegisterNodeRequest,
- ) -> Result<CommonReply, RobustMQError> {
-     let mut client = match PlacementCenterServiceClient::connect(format!("http://{}", addr)).await {
-         Ok(client) => client,
-         Err(err) => return Err(RobustMQError::TonicTransport(err)),
-     };
- 
-     let resp = match client.un_register_node(tonic::Request::new(request)).await {
-         Ok(reply) => reply.into_inner(),
-         Err(status) => return Err(RobustMQError::MetaGrpcStatus(status)),
-     };
-     return Ok(resp);
- }
- 
- pub async fn create_shard(
-     addr: &String,
-     request: CreateShardRequest,
- ) -> Result<CommonReply, RobustMQError> {
-     let mut client = match PlacementCenterServiceClient::connect(format!("http://{}", addr)).await {
-         Ok(client) => client,
-         Err(err) => return Err(RobustMQError::TonicTransport(err)),
-     };
- 
-     let resp = match client.create_shard(tonic::Request::new(request)).await {
-         Ok(reply) => reply.into_inner(),
-         Err(status) => return Err(RobustMQError::MetaGrpcStatus(status)),
-     };
-     return Ok(resp);
- }
- 
- pub async fn delete_shard(
-     addr: &String,
-     request: DeleteShardRequest,
- ) -> Result<CommonReply, RobustMQError> {
-     let mut client = match PlacementCenterServiceClient::connect(format!("http://{}", addr)).await {
-         Ok(client) => client,
-         Err(err) => return Err(RobustMQError::TonicTransport(err)),
-     };
- 
-     let resp = match client.delete_shard(tonic::Request::new(request)).await {
-         Ok(reply) => reply.into_inner(),
-         Err(status) => return Err(RobustMQError::MetaGrpcStatus(status)),
-     };
-     return Ok(resp);
- }
+use common::errors::RobustMQError;
+use protocol::placement_center::placement::{
+    placement_center_service_client::PlacementCenterServiceClient, CommonReply, CreateShardRequest,
+    DeleteShardRequest, HeartbeatRequest, RegisterNodeRequest, SendRaftConfChangeReply,
+    SendRaftConfChangeRequest, SendRaftMessageReply, SendRaftMessageRequest, UnRegisterNodeRequest,
+};
+use tonic::transport::Channel;
 
- pub async fn heartbeat(
+pub async fn build_placement_center_client(
+    addr: &String,
+) -> Result<PlacementCenterServiceClient<Channel>, RobustMQError> {
+    match PlacementCenterServiceClient::connect(format!("http://{}", addr)).await {
+        Ok(client) => return Ok(client),
+        Err(err) => return Err(RobustMQError::TonicTransport(err)),
+    };
+}
+
+pub async fn register_node(
+    addr: &String,
+    request: RegisterNodeRequest,
+) -> Result<CommonReply, RobustMQError> {
+    let mut client = match PlacementCenterServiceClient::connect(format!("http://{}", addr)).await {
+        Ok(client) => client,
+        Err(err) => return Err(RobustMQError::TonicTransport(err)),
+    };
+
+    let resp = match client.register_node(tonic::Request::new(request)).await {
+        Ok(reply) => reply.into_inner(),
+        Err(status) => return Err(RobustMQError::MetaGrpcStatus(status)),
+    };
+    return Ok(resp);
+}
+
+pub async fn unregister_node(
+    addr: &String,
+    request: UnRegisterNodeRequest,
+) -> Result<CommonReply, RobustMQError> {
+    let mut client = match PlacementCenterServiceClient::connect(format!("http://{}", addr)).await {
+        Ok(client) => client,
+        Err(err) => return Err(RobustMQError::TonicTransport(err)),
+    };
+
+    let resp = match client.un_register_node(tonic::Request::new(request)).await {
+        Ok(reply) => reply.into_inner(),
+        Err(status) => return Err(RobustMQError::MetaGrpcStatus(status)),
+    };
+    return Ok(resp);
+}
+
+pub async fn create_shard(
+    addr: &String,
+    request: CreateShardRequest,
+) -> Result<CommonReply, RobustMQError> {
+    let mut client = match PlacementCenterServiceClient::connect(format!("http://{}", addr)).await {
+        Ok(client) => client,
+        Err(err) => return Err(RobustMQError::TonicTransport(err)),
+    };
+
+    let resp = match client.create_shard(tonic::Request::new(request)).await {
+        Ok(reply) => reply.into_inner(),
+        Err(status) => return Err(RobustMQError::MetaGrpcStatus(status)),
+    };
+    return Ok(resp);
+}
+
+pub async fn delete_shard(
+    addr: &String,
+    request: DeleteShardRequest,
+) -> Result<CommonReply, RobustMQError> {
+    let mut client = match PlacementCenterServiceClient::connect(format!("http://{}", addr)).await {
+        Ok(client) => client,
+        Err(err) => return Err(RobustMQError::TonicTransport(err)),
+    };
+
+    let resp = match client.delete_shard(tonic::Request::new(request)).await {
+        Ok(reply) => reply.into_inner(),
+        Err(status) => return Err(RobustMQError::MetaGrpcStatus(status)),
+    };
+    return Ok(resp);
+}
+
+pub async fn heartbeat(
     addr: &String,
     request: HeartbeatRequest,
 ) -> Result<CommonReply, RobustMQError> {
@@ -98,38 +110,38 @@
     };
     return Ok(resp);
 }
- 
- pub async fn send_raft_message(
-     addr: &String,
-     message: Vec<u8>,
- ) -> Result<SendRaftMessageReply, RobustMQError> {
-     let mut client = match PlacementCenterServiceClient::connect(format!("http://{}", addr)).await {
-         Ok(client) => client,
-         Err(err) => return Err(RobustMQError::TonicTransport(err)),
-     };
-     let request = tonic::Request::new(SendRaftMessageRequest { message });
- 
-     let resp = match client.send_raft_message(request).await {
-         Ok(reply) => reply.into_inner(),
-         Err(status) => return Err(RobustMQError::MetaGrpcStatus(status)),
-     };
-     return Ok(resp);
- }
- 
- pub async fn send_raft_conf_change(
-     addr: &String,
-     message: Vec<u8>,
- ) -> Result<SendRaftConfChangeReply, RobustMQError> {
-     let mut client = match PlacementCenterServiceClient::connect(format!("http://{}", addr)).await {
-         Ok(client) => client,
-         Err(err) => return Err(RobustMQError::TonicTransport(err)),
-     };
-     let request = tonic::Request::new(SendRaftConfChangeRequest { message });
- 
-     let resp = match client.send_raft_conf_change(request).await {
-         Ok(reply) => reply.into_inner(),
-         Err(status) => return Err(RobustMQError::MetaGrpcStatus(status)),
-     };
-     return Ok(resp);
- }
- 
+
+pub async fn send_raft_message(
+    addr: &String,
+    message: Vec<u8>,
+) -> Result<SendRaftMessageReply, RobustMQError> {
+    let mut client = match PlacementCenterServiceClient::connect(format!("http://{}", addr)).await {
+        Ok(client) => client,
+        Err(err) => return Err(RobustMQError::TonicTransport(err)),
+    };
+    let request = tonic::Request::new(SendRaftMessageRequest { message });
+
+    let resp = match client.send_raft_message(request).await {
+        Ok(reply) => reply.into_inner(),
+        Err(status) => return Err(RobustMQError::MetaGrpcStatus(status)),
+    };
+    return Ok(resp);
+}
+
+pub async fn send_raft_conf_change(
+    addr: &String,
+    message: Vec<u8>,
+) -> Result<SendRaftConfChangeReply, RobustMQError> {
+    let mut client: PlacementCenterServiceClient<Channel> =
+        match PlacementCenterServiceClient::connect(format!("http://{}", addr)).await {
+            Ok(client) => client,
+            Err(err) => return Err(RobustMQError::TonicTransport(err)),
+        };
+    let request = tonic::Request::new(SendRaftConfChangeRequest { message });
+
+    let resp = match client.send_raft_conf_change(request).await {
+        Ok(reply) => reply.into_inner(),
+        Err(status) => return Err(RobustMQError::MetaGrpcStatus(status)),
+    };
+    return Ok(resp);
+}
