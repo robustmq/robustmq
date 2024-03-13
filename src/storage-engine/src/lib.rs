6bed814f90d733a007d3305cdfa61555a836ba44
loboxu <loboxu@tencent.com>
2024-03-13 23:41:15 +0800

    dev

--- src/storage-engine/src/lib.rs
+++ src/storage-engine/src/lib.rs
@@ -7,7 +7,6 @@ use common::{
     metrics::register_prometheus_export, runtime::create_runtime,
 };
 use protocol::storage_engine::storage::storage_engine_service_server::StorageEngineServiceServer;
-use services::StorageService;
 use tokio::{
     runtime::Runtime,
     signal,
@@ -21,11 +20,8 @@ mod metadata;
 mod raft_group;
 mod record;
 mod segment;
-mod services;
 mod shard;
 mod storage;
-mod v1;
-mod v2;
 
 pub struct StorageEngine {
     config: StorageEngineConfig,
@@ -54,7 +50,6 @@ impl StorageEngine {
     }
 
     pub fn start(&self) {
-
         self.register_node();
 
         self.start_server();
@@ -68,21 +63,6 @@ impl StorageEngine {
     fn start_server(&self) {
         // start grpc server
         let port = self.config.grpc_port;
-        self.server_runtime.spawn(async move {
-            let ip = format!("0.0.0.0:{}", port).parse().unwrap();
-            info_meta(&format!(
-                "RobustMQ StorageEngine Grpc Server start success. bind port:{}",
-                ip
-            ));
-
-            let service_handler = StorageService::new();
-
-            Server::builder()
-                .add_service(StorageEngineServiceServer::new(service_handler))
-                .serve(ip)
-                .await
-                .unwrap();
-        });
 
         // start prometheus http server
         let prometheus_port = self.config.prometheus_port;
