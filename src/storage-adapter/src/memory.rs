9b85eb4c3c20938b3a96e2522ca7c6995a3abd31
loboxu.1227 <loboxu.1227@bytedance.com>
2024-04-11 14:18:32 +0800

    memory storage index

--- src/storage-adapter/src/memory.rs
+++ src/storage-adapter/src/memory.rs
@@ -1,63 +1,126 @@
 use crate::storage::StorageAdapter;
 use axum::async_trait;
 use common_base::{errors::RobustMQError, log::info};
+use dashmap::DashMap;
 
 #[derive(Clone)]
-pub struct MemoryStorageAdapter {}
+pub struct MemoryStorageAdapter {
+    pub memory_data: DashMap<String, String>,
+    pub shard_data: DashMap<String, Vec<Vec<u8>>>,
+    pub group_data: DashMap<String, usize>,
+}
 
 impl MemoryStorageAdapter {
     pub fn new() -> Self {
-        return MemoryStorageAdapter {};
+        return MemoryStorageAdapter {
+            memory_data: DashMap::with_capacity(256),
+            shard_data: DashMap::with_capacity(256),
+            group_data: DashMap::with_capacity(256),
+        };
     }
 }
 
 #[async_trait]
 impl StorageAdapter for MemoryStorageAdapter {
     async fn kv_set(&self, key: String, value: String) -> Result<(), RobustMQError> {
+        self.memory_data.insert(key, value);
         return Ok(());
     }
-    async fn kv_get(&self, key: String) -> Result<String, RobustMQError> {
-        info(format!("kv_get:{}", key));
-        return Ok("".to_string());
+    async fn kv_get(&self, key: String) -> Option<String> {
+        if let Some(data) = self.memory_data.get(&key) {
+            return Some(data.to_string());
+        }
+        return None;
     }
     async fn kv_delete(&self, key: String) -> Result<(), RobustMQError> {
-        info(format!("kv_delete:{}", key));
+        self.memory_data.remove(&key);
         return Ok(());
     }
     async fn kv_exists(&self, key: String) -> Result<bool, RobustMQError> {
-        info(format!("kv_exists:{}", key));
-        return Ok(true);
+        return Ok(self.memory_data.contains_key(&key));
     }
 
     async fn stream_write(
         &self,
         shard_name: String,
-        bytess: Vec<u8>,
-    ) -> Result<u128, RobustMQError> {
-        info(format!("stream_write:"));
-        return Ok(1);
+        bytes: Vec<u8>,
+    ) -> Result<usize, RobustMQError> {
+        let mut shard = if let Some((_, da)) = self.shard_data.remove(&shard_name) {
+            da
+        } else {
+            Vec::new()
+        };
+        shard.push(bytes);
+        let offset = shard.len() - 1;
+        self.shard_data.insert(shard_name, shard);
+
+        // build timestamp index
+
+        // build key index
+        return Ok(offset);
     }
 
-    async fn stream_read_next(&self, shard_name: String) -> Result<Vec<u8>, RobustMQError> {
-        info(format!("stream_read"));
-        return Ok("".to_string().into_bytes());
+    async fn stream_read_next(
+        &self,
+        shard_name: String,
+        group_id: String,
+    ) -> Result<Option<Vec<u8>>, RobustMQError> {
+        let offset = if let Some(da) = self.group_data.get(&group_id) {
+            *da
+        } else {
+            0
+        };
+
+        if let Some(da) = self.shard_data.get(&shard_name) {
+            if let Some(value) = da.get(offset) {
+                self.group_data.insert(shard_name, offset + 1);
+                return Ok(Some(value.clone()));
+            }
+        }
+        return Ok(None);
     }
-    
+
     async fn stream_read_next_batch(
         &self,
         shard_name: String,
-        record_num: u16,
-    ) -> Result<Vec<Vec<u8>>, RobustMQError> {
-        info(format!("stream_read"));
-        return Ok(Vec::new());
+        group_id: String,
+        record_num: usize,
+    ) -> Result<Option<Vec<Vec<u8>>>, RobustMQError> {
+        let offset = if let Some(da) = self.group_data.get(&group_id) {
+            *da
+        } else {
+            0
+        };
+        if let Some(da) = self.shard_data.get(&shard_name) {
+            let mut cur_offset = 0;
+            let mut result = Vec::new();
+            for i in offset..(offset + record_num) {
+                if let Some(value) = da.get(i) {
+                    result.push(value.clone());
+                    cur_offset += 1;
+                } else {
+                    break;
+                }
+            }
+            if cur_offset > 0 {
+                self.group_data.insert(shard_name, offset + cur_offset);
+            }
+            return Ok(Some(result));
+        }
+        return Ok(None);
     }
 
-    async fn stream_read_by_id(
+    async fn stream_read_by_offset(
         &self,
         shard_name: String,
-        record_id: u128,
-    ) -> Result<Vec<u8>, RobustMQError> {
-        return Ok("".to_string().into_bytes());
+        offset: usize,
+    ) -> Result<Option<Vec<u8>>, RobustMQError> {
+        if let Some(da) = self.shard_data.get(&shard_name) {
+            if let Some(value) = da.get(offset) {
+                return Ok(Some(value.clone()));
+            }
+        }
+        return Ok(None);
     }
 
     async fn stream_read_by_timestamp(
@@ -65,15 +128,15 @@ impl StorageAdapter for MemoryStorageAdapter {
         shard_name: String,
         start_timestamp: u128,
         end_timestamp: u128,
-    ) -> Result<Vec<Vec<u8>>, RobustMQError> {
-        return Ok(Vec::new());
+    ) -> Result<Option<Vec<Vec<u8>>>, RobustMQError> {
+        return Ok(None);
     }
 
-    async fn stream_read_by_last_expiry(
+    async fn stream_read_by_key(
         &self,
         shard_name: String,
-        second: u128,
-    ) -> Result<Vec<Vec<u8>>, RobustMQError> {
-        return Ok(Vec::new());
+        key: String,
+    ) -> Result<Option<Vec<u8>>, RobustMQError> {
+        return Ok(None);
     }
 }
--- src/storage-adapter/src/storage.rs
+++ src/storage-adapter/src/storage.rs
@@ -4,29 +4,45 @@ use common_base::errors::RobustMQError;
 #[async_trait]
 pub trait StorageAdapter {
     // kv storage model: Set data
-    async fn  kv_set(&self, key: String, value: String) -> Result<(), RobustMQError>;
+    async fn kv_set(&self, key: String, value: String) -> Result<(), RobustMQError>;
+    
     // kv storage model: Get data
-    async fn kv_get(&self, key: String) -> Result<String, RobustMQError>;
+    async fn kv_get(&self, key: String) -> Option<String>;
+    
     // kv storage model: Delete data
     async fn kv_delete(&self, key: String) -> Result<(), RobustMQError>;
+    
     // kv storage model: Determines whether the key exists
     async fn kv_exists(&self, key: String) -> Result<bool, RobustMQError>;
+    
     // Streaming storage model: Append data in a Shard dimension, returning a unique self-incrementing ID for the Shard dimension
-    async fn stream_write(&self, shard_name: String, bytess: Vec<u8>) -> Result<u128, RobustMQError>;
+    async fn stream_write(
+        &self,
+        shard_name: String,
+        bytes: Vec<u8>,
+    ) -> Result<usize, RobustMQError>;
+    
     // Streaming storage model: Read the next item in the dimension of the Shard + subscription name tuple
-    async fn stream_read_next(&self, shard_name: String) -> Result<Vec<u8>, RobustMQError>;
+    async fn stream_read_next(
+        &self,
+        shard_name: String,
+        group_id: String,
+    ) -> Result<Option<Vec<u8>>, RobustMQError>;
+
     // Streaming storage model: Read the next batch of data in the dimension of the Shard + subscription name tuple
     async fn stream_read_next_batch(
         &self,
         shard_name: String,
-        record_num: u16,
-    ) -> Result<Vec<Vec<u8>>, RobustMQError>;
+        group_id: String,
+        record_num: usize,
+    ) -> Result<Option<Vec<Vec<u8>>>, RobustMQError>;
+
     // Streaming storage model: A piece of data is uniquely read based on the shard name and a unique auto-incrementing ID.
-    async fn stream_read_by_id(
+    async fn stream_read_by_offset(
         &self,
         shard_name: String,
-        record_id: u128,
-    ) -> Result<Vec<u8>, RobustMQError>;
+        record_id: usize,
+    ) -> Result<Option<Vec<u8>>, RobustMQError> ;
 
     // Streaming storage model: A batch of data is read based on the shard name and time range.
     async fn stream_read_by_timestamp(
@@ -34,12 +50,12 @@ pub trait StorageAdapter {
         shard_name: String,
         start_timestamp: u128,
         end_timestamp: u128,
-    ) -> Result<Vec<Vec<u8>>, RobustMQError>;
+    ) -> Result<Option<Vec<Vec<u8>>>, RobustMQError>;
 
     // Streaming storage model: A batch of data is read based on the shard name and the last time it expires
-    async fn stream_read_by_last_expiry(
+    async fn stream_read_by_key(
         &self,
         shard_name: String,
-        second: u128,
-    ) -> Result<Vec<Vec<u8>>, RobustMQError>;
+        key: String,
+    ) -> Result<Option<Vec<u8>>, RobustMQError>;
 }
