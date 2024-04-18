bb91abefbc8ffbb96759b195a905b299bf714d11
loboxu <loboxu@tencent.com>
2024-04-18 09:45:00 +0800

    dev

--- src/mqtt-broker/src/security/security.rs
+++ src/mqtt-broker/src/security/security.rs
@@ -1,6 +1,23 @@
 use bytes::Bytes;
+use common_base::errors::RobustMQError;
 use protocol::mqtt::Login;
 
 pub trait Authentication {
-    fn apply(login: Option<Login>, authentication_data: Option<Bytes>);
+    fn apply(
+        login: Option<Login>,
+        authentication_data: Option<Bytes>,
+    ) -> Result<bool, RobustMQError>;
+}
+
+pub fn authentication_login(
+    login: Option<Login>,
+    authentication_method: Option<String>,
+    authentication_data: Option<Bytes>,
+) -> Result<bool, RobustMQError> {
+    
+    if let Some(info) = login {
+        return Ok(false);
+    }
+
+    return Ok(true);
 }
