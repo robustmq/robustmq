39e9626cf3ef2b29caa63d7097d71e7f897835c4
loboxu <loboxu@tencent.com>
2024-04-19 08:43:05 +0800

    dev

--- src/mqtt-broker/src/metadata/user.rs
+++ src/mqtt-broker/src/metadata/user.rs
@@ -1,7 +1,10 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Clone,Serialize, Deserialize)]
+#[derive(Clone, Serialize, Deserialize)]
 pub struct User {
     pub username: String,
     pub password: String,
+    pub salt: u16,
+    pub is_superuser: bool,
+    pub create_time: u128,
 }
