bffcab3b1aa61d112de86759d00f9be6754052fe
loboxu <loboxu@tencent.com>
2024-04-21 22:23:27 +0800

    mysql storage adapter

--- src/storage-adapter/src/mysql/mod.rs
+++ src/storage-adapter/src/mysql/mod.rs
@@ -1,15 +1,168 @@
-use std::sync::Arc;
+use crate::{record::Record, storage::StorageAdapter};
+use axum::async_trait;
+use common_base::{errors::RobustMQError, tools::now_mills};
+use mysql::{params, prelude::Queryable, Pool, PooledConn};
 
-use clients::ClientPool;
-use tokio::sync::Mutex;
+use self::schema::TMqttKvMsg;
+pub mod schema;
 
+// addr: mysql://root:password@localhost:3307/db_name
 #[derive(Clone)]
 pub struct MySQLStorageAdapter {
-    addrs: String,
+    pool: Pool,
 }
 
 impl MySQLStorageAdapter {
-    pub fn new(client_poll: Arc<Mutex<ClientPool>>, addrs: Vec<String>) -> Self {
-        return MySQLStorageAdapter { client_poll, addrs };
+    pub fn new(pool: Pool) -> Self {
+        return MySQLStorageAdapter { pool };
+    }
+
+    pub fn insert(&self) {
+        // conn.ex
+    }
+
+    pub fn drop(&self) {}
+
+    pub fn update(&self) {}
+
+    pub fn select(&self) {}
+}
+
+#[async_trait]
+impl StorageAdapter for MySQLStorageAdapter {
+    async fn set(&self, key: String, value: Record) -> Result<(), RobustMQError> {
+        match self.pool.get_conn() {
+            Ok(mut conn) => {
+                let values = vec![TMqttKvMsg {
+                    key,
+                    value: String::from_utf8(value.data).unwrap(),
+                    create_time: now_mills(),
+                }];
+                match conn.exec_batch(
+                    r"INSERT INTO payment (key, value, create_time)
+                      VALUES (:key, :value, :create_time)",
+                    values.iter().map(|p| {
+                        params! {
+                            "key" => p.key.clone(),
+                            "value" => p.value.clone(),
+                            "create_time" => p.create_time,
+                        }
+                    }),
+                ) {
+                    Ok(_) => {
+                        return Ok(());
+                    }
+                    Err(e) => {
+                        return Err(RobustMQError::CommmonError(e.to_string()));
+                    }
+                }
+            }
+            Err(e) => {
+                return Err(RobustMQError::CommmonError(e.to_string()));
+            }
+        }
+    }
+
+    async fn get(&self, key: String) -> Result<Option<Record>, RobustMQError> {
+        return Err(RobustMQError::NotSupportFeature(
+            "PlacementStorageAdapter".to_string(),
+            "stream_write".to_string(),
+        ));
+    }
+    async fn delete(&self, key: String) -> Result<(), RobustMQError> {
+        return Ok(());
+    }
+    async fn exists(&self, key: String) -> Result<bool, RobustMQError> {
+        return Err(RobustMQError::NotSupportFeature(
+            "PlacementStorageAdapter".to_string(),
+            "stream_write".to_string(),
+        ));
+    }
+
+    async fn stream_write(&self, _: String, _: Vec<Record>) -> Result<Vec<usize>, RobustMQError> {
+        return Err(RobustMQError::NotSupportFeature(
+            "PlacementStorageAdapter".to_string(),
+            "stream_write".to_string(),
+        ));
+    }
+
+    async fn stream_read(
+        &self,
+        _: String,
+        _: String,
+        _: Option<u128>,
+        _: Option<usize>,
+    ) -> Result<Option<Vec<Record>>, RobustMQError> {
+        return Err(RobustMQError::NotSupportFeature(
+            "PlacementStorageAdapter".to_string(),
+            "stream_write".to_string(),
+        ));
+    }
+
+    async fn stream_commit_offset(
+        &self,
+        _: String,
+        _: String,
+        _: u128,
+    ) -> Result<bool, RobustMQError> {
+        return Err(RobustMQError::NotSupportFeature(
+            "PlacementStorageAdapter".to_string(),
+            "stream_write".to_string(),
+        ));
+    }
+
+    async fn stream_read_by_offset(
+        &self,
+        _: String,
+        _: usize,
+    ) -> Result<Option<Record>, RobustMQError> {
+        return Err(RobustMQError::NotSupportFeature(
+            "PlacementStorageAdapter".to_string(),
+            "stream_write".to_string(),
+        ));
+    }
+
+    async fn stream_read_by_timestamp(
+        &self,
+        _: String,
+        _: u128,
+        _: u128,
+        _: Option<usize>,
+        _: Option<usize>,
+    ) -> Result<Option<Vec<Record>>, RobustMQError> {
+        return Err(RobustMQError::NotSupportFeature(
+            "PlacementStorageAdapter".to_string(),
+            "stream_write".to_string(),
+        ));
+    }
+
+    async fn stream_read_by_key(
+        &self,
+        _: String,
+        _: String,
+    ) -> Result<Option<Record>, RobustMQError> {
+        return Err(RobustMQError::NotSupportFeature(
+            "PlacementStorageAdapter".to_string(),
+            "stream_write".to_string(),
+        ));
     }
 }
+
+fn build_mysql_conn_pool(addr: &str) -> Result<PooledConn, RobustMQError> {
+    match Pool::new(addr) {
+        Ok(pool) => match pool.get_conn() {
+            Ok(conn) => {
+                return Ok(conn);
+            }
+            Err(e) => {
+                return Err(RobustMQError::CommmonError(e.to_string()));
+            }
+        },
+        Err(e) => {
+            return Err(RobustMQError::CommmonError(e.to_string()));
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {}
---
+++ src/storage-adapter/src/mysql/schema.rs
@@ -0,0 +1,16 @@
+#[derive(Debug, PartialEq, Eq)]
+pub struct TMqttStreamMsg {
+    pub id: u32,
+    pub msgid: String,
+    pub shard: String,
+    pub qos: u16,
+    pub payload: String,
+    pub create_time: u64,
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct TMqttKvMsg {
+    pub key: String,
+    pub value: String,
+    pub create_time: u128,
+}
