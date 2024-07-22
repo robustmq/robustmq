189739bb8cf836250b0caee53fe9bd9f8ec0e40c
loboxu.1227 <loboxu.1227@bytedance.com>
2024-07-22 20:48:49 +0800

    dev

--- src/mqtt-broker/src/server/tcp/server.rs
+++ src/mqtt-broker/src/server/tcp/server.rs
@@ -317,6 +317,7 @@ where
                         }
                     },
                     val = request_queue_rx.recv()=>{
+                        info(format!("start{}",request_queue_rx.len()));
                         if let Some(packet) = val{
                             // Try to deliver the request packet to the child handler until it is delivered successfully.
                             // Because some request queues may be full or abnormal, the request packets can be delivered to other child handlers.
@@ -348,6 +349,7 @@ where
                             }
 
                         }
+                        info(format!("end{}",request_queue_rx.len()));
                     }
                 }
             }
