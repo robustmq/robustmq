98329458cc71e0d88b31a20163026d9350236723
loboxu <loboxu@tencent.com>
2024-07-30 13:53:42 +0800

    dev

--- src/placement-center/src/core/share_sub.rs
+++ src/placement-center/src/core/share_sub.rs
@@ -1,15 +1,51 @@
-use crate::cache::placement::PlacementCacheManager;
+use crate::{
+    cache::placement::PlacementCacheManager,
+    storage::{
+        keys::storage_key_mqtt_sub_group_leader, placement::kv::KvStorage, rocksdb::RocksDBEngine,
+    },
+};
 use common_base::errors::RobustMQError;
 use std::sync::Arc;
 
 pub fn calc_share_sub_leader(
-    cluster_name: String,
-    group_name: String,
+    cluster_name: &String,
+    group_name: &String,
     cluster_cache: Arc<PlacementCacheManager>,
+    rocksdb_engine_handler: Arc<RocksDBEngine>,
 ) -> Result<u64, RobustMQError> {
-    if let Some(cluster) = cluster_cache.node_list.get(&cluster_name) {
-        // todo The node where the Share Sub Leader is located is calculated based on the cluster load
-        return Ok(cluster.first().unwrap().clone());
+    let key = storage_key_mqtt_sub_group_leader(cluster_name, group_name);
+    let kv_storage = KvStorage::new(rocksdb_engine_handler);
+
+    if let Some(data) = kv_storage.get(key) {
+        match data.parse::<u64>() {
+            Ok(da) => {
+                if let Some(node_list) = cluster_cache.node_list.get(cluster_name) {
+                    if node_list.contains_key(&da) {
+                        return Ok(da);
+                    }
+                }
+            }
+            Err(_) => {}
+        }
     }
-    return Err(RobustMQError::ClusterDoesNotExist(cluster_name));
+
+    let mut broker_ids = Vec::new();
+    if let Some(cluster) = cluster_cache.node_list.get(cluster_name) {
+        for (id, _) in cluster.clone() {
+            broker_ids.push(id);
+        }
+    }
+    broker_ids.sort();
+    if broker_ids.len() == 0 {
+        return Err(RobustMQError::ClusterDoesNotExist(cluster_name.clone()));
+    }
+    return Ok(broker_ids.first().unwrap().clone());
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn calc_share_sub_leader_test() {
+        
+    }
 }
