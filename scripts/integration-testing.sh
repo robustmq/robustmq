b954ee6b9f5b9fc67a9cabb7cc9b12e7babb930d
loboxu.1227 <loboxu.1227@bytedance.com>
2024-08-08 21:35:10 +0800

    RBIP-09：A makefile for writing integration tests

--- scripts/integration-testing.sh
+++ scripts/integration-testing.sh
@@ -8,7 +8,7 @@ mqtt_server_process_name="mqtt-server"
 
 # Start placement-center
 nohup cargo run --package cmd --bin $placement_center_process_name -- --conf=tests/config/$placement_center_process_name.toml >/dev/null 2>&1 &
-sleep 5
+sleep 3
 while ! ps aux | grep -v grep | grep "$placement_center_process_name" > /dev/null; do
     echo "Process $placement_center_process_name has not started yet, wait 1s...."
     sleep 1  
@@ -17,7 +17,7 @@ echo "Process $placement_center_process_name starts successfully and starts runn
 
 # Start mqtt-broker
 nohup cargo run --package cmd --bin $mqtt_server_process_name -- --conf=tests/config/$mqtt_server_process_name.toml >/dev/null 2>&1 &
-sleep 5
+sleep 3
 while ! ps aux | grep -v grep | grep "$mqtt_server_process_name" > /dev/null; do
     echo "Process $mqtt_server_process_name has not started yet, wait 1s...."
     sleep 1  
