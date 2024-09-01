8a2b5f9f8961baedce2e51cd65d2a437baf32d3b
loboxu.1227 <loboxu.1227@bytedance.com>
2024-09-01 23:54:54 +0800

    mqtt broker dev

---
+++ src/mqtt-broker/src/handler/system_topic.rs
@@ -0,0 +1,6 @@
+pub const SYSTEM_TOPIC_BROKERS: &str = "$SYS/brokers";
+pub const SYSTEM_TOPIC_BROKERS_VERSION: &str = "$SYS/brokers/${node}/version";
+pub const SYSTEM_TOPIC_BROKERS_UPTIME: &str = "$SYS/brokers/${node}/uptime";
+pub const SYSTEM_TOPIC_BROKERS_DATETIME: &str = "$SYS/brokers/${node}/datetime";
+pub const SYSTEM_TOPIC_BROKERS_SYSDESCR: &str = "$SYS/brokers/${node}/sysdescr";
+pub const SYSTEM_TOPIC_BROKERS_CLIENTS: &str = "$SYS/brokers/${node}/clients";
