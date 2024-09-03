5c3d8ef50444cdcfdc704895cb61526a8b9de870
huichungka <498670459@163.com>
2024-09-04 00:50:28 +0800

    MINOR：Add clients module placement/mqtt Session related test cases #375

--- src/clients/tests/mqtt_session_test.rs
+++ src/clients/tests/mqtt_session_test.rs
@@ -22,16 +22,26 @@ mod tests {
         let client_id: String = "test_cluster_id".to_string();
         let connection_id: u64 = 1;
         let broker_id: u64 = 1;
-        let reconnect_time: u64 = 10000000;
-        let distinct_time: u64 = 10000000;
+        let update_broker_id: u64 = 2;
+        let session_expiry: u64 = 10000;
+        let last_will_delay_interval: u64 = 10000;
 
-        let mqtt_session: MQTTSession = MQTTSession::default();
+        let mut mqtt_session: MQTTSession = MQTTSession::new(
+            &client_id,
+            session_expiry.clone(),
+            true,
+            Some(last_will_delay_interval.clone())
+        );
 
+        mqtt_session.update_broker_id(Some(broker_id.clone()));
+
+        
         let request = CreateSessionRequest {
             cluster_name: cluster_name.clone(),
             client_id: client_id.clone(),
             session: MQTTSession::encode(&mqtt_session),
         };
+        println!("CreateSessionRequest: {:?}", request);
 
         match placement_create_session(client_poll.clone(), addrs.clone(), request).await {
             Ok(_) => {}
@@ -59,14 +69,19 @@ mod tests {
             }
         }
 
+        mqtt_session.update_broker_id(Some(update_broker_id.clone()));
+        mqtt_session.update_reconnect_time();
+        mqtt_session.update_distinct_time();
+
         let request = UpdateSessionRequest {
             cluster_name: cluster_name.clone(),
             client_id: mqtt_session.client_id.clone(),
-            connection_id: connection_id.clone(),
-            broker_id: broker_id.clone(),
-            reconnect_time: reconnect_time.clone(),
-            distinct_time: distinct_time.clone(),
+            connection_id: mqtt_session.connection_id.unwrap_or(connection_id.clone()),
+            broker_id: mqtt_session.broker_id.unwrap_or(1100),
+            reconnect_time: mqtt_session.reconnect_time.unwrap(),
+            distinct_time: mqtt_session.distinct_time.unwrap(),
         };
+        println!("UpdateSessionRequest: {:?}", request);
 
         match placement_update_session(client_poll.clone(), addrs.clone(), request).await {
             Ok(_) => {}
@@ -98,6 +113,7 @@ mod tests {
             cluster_name: cluster_name.clone(),
             client_id: mqtt_session.client_id.clone(),
         };
+        println!("DeleteSessionRequest: {:?}", request);
 
         match placement_delete_session(client_poll.clone(), addrs.clone(), request).await {
             Ok(_) => {}
