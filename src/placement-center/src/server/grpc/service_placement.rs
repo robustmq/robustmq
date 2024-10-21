1c9da8f15a1c58f75a6d8a990cde364e35e88107
Soumya Sen <57633153+soumyasen1809@users.noreply.github.com>
2024-10-21 16:46:11 +0000

    refactor: Add validation to delete_idempotent_data
    Add validation to the delete_idempotent_data method in GrpcPlacementService

--- src/placement-center/src/server/grpc/service_placement.rs
+++ src/placement-center/src/server/grpc/service_placement.rs
@@ -294,6 +294,7 @@ impl PlacementCenterService for GrpcPlacementService {
         request: Request<DeleteIdempotentDataRequest>,
     ) -> Result<Response<DeleteIdempotentDataReply>, Status> {
         let req = request.into_inner();
+        let _ = req.validate_ext()?;
         let data = StorageData::new(
             StorageDataType::ClusterDeleteIdempotentData,
             DeleteIdempotentDataRequest::encode_to_vec(&req),
