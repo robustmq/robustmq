750b376ee3f795cfc163652dace932c40d0fb049
liuke <keyang.lk@antgroup.com>
2024-10-22 20:22:32 +0800

    feat：add Subscription Options test cases

--- src/mqtt-broker/tests/sub_options_test.rs
+++ src/mqtt-broker/tests/sub_options_test.rs
@@ -16,63 +16,169 @@ mod common;
 
 #[cfg(test)]
 mod tests {
+    use std::time::Duration;
 
     use common_base::tools::unique_id;
-    use paho_mqtt::{Message, QOS_1};
+    use paho_mqtt::{Message, RetainHandling, SubscribeOptions, QOS_0, QOS_2};
 
     use crate::common::{broker_addr, connect_server5, distinct_conn};
 
     #[tokio::test]
-    async fn sub_options_test() {
-        let sub_qos = &[0];
+    async fn sub_options_no_local_test() {
         let topic = format!("/tests/{}", unique_id());
-        simple_test(topic.clone(), topic.clone(), sub_qos, "2".to_string()).await;
+        no_local_test(topic.clone(), topic.clone(), true, "1".to_string()).await;
 
-        let sub_qos = &[1];
         let topic = format!("/tests/{}", unique_id());
-        simple_test(topic.clone(), topic.clone(), sub_qos, "1".to_string()).await;
-
-        let sub_qos = &[2];
-        let topic = format!("/tests/{}", unique_id());
-        simple_test(topic.clone(), topic.clone(), sub_qos, "3".to_string()).await;
+        no_local_test(topic.clone(), topic.clone(), false, "0".to_string()).await;
     }
 
-    async fn simple_test(
+    #[tokio::test]
+    async fn sub_options_retain_as_published_test() {
+        let topic = format!("/tests/{}", unique_id());
+        retain_as_published_test(topic.clone(), topic.clone(), true, "1".to_string()).await;
+
+        let topic = format!("/tests/{}", unique_id());
+        retain_as_published_test(topic.clone(), topic.clone(), false, "0".to_string()).await;
+    }
+
+    #[tokio::test]
+    async fn sub_options_retain_handling_test() {
+        let mut i = 0;
+        for retain in vec![
+            RetainHandling::SendRetainedOnSubscribe,
+            RetainHandling::SendRetainedOnNew,
+            RetainHandling::DontSendRetained,
+        ] {
+            let topic = format!("/tests/{}", unique_id());
+            retain_handling_test(topic.clone(), topic.clone(), retain, i.to_string()).await;
+            i += 1;
+        }
+    }
+
+    async fn no_local_test(
         pub_topic: String,
         sub_topic: String,
-        sub_qos: &[i32],
+        no_local: bool,
         payload_flag: String,
     ) {
         let client_id = unique_id();
         let addr = broker_addr();
         let sub_topics = &[sub_topic.clone()];
+        let sub_qos = &[QOS_2];
+        let sub_opts = &[SubscribeOptions::new(no_local, false, None)];
 
         let cli = connect_server5(&client_id, &addr, false, false);
-        let message_content = format!("mqtt {payload_flag} message");
 
+        let message_content = format!("mqtt {payload_flag} message");
         // publish
-        let msg = Message::new(pub_topic.clone(), message_content.clone(), QOS_1);
-        match cli.publish(msg) {
-            Ok(_) => {}
-            Err(e) => {
-                panic!("{:?}", e);
-            }
-        }
+        let msg = Message::new(pub_topic.clone(), message_content.clone(), QOS_2);
+        assert!(cli.publish(msg).is_ok());
 
         // subscribe
         let rx = cli.start_consuming();
-        match cli.subscribe_many(sub_topics, sub_qos) {
-            Ok(_) => {}
-            Err(e) => {
-                panic!("{}", e)
+        assert!(cli
+            .subscribe_many_with_options(sub_topics, sub_qos, sub_opts, None)
+            .is_ok());
+
+        match rx.recv_timeout(Duration::from_secs(1)) {
+            Ok(msg) => {
+                assert!(!no_local);
+                let payload = String::from_utf8(msg.unwrap().payload().to_vec()).unwrap();
+                assert_eq!(payload, message_content);
+            }
+            Err(_) => {
+                assert!(no_local);
+            }
+        };
+        distinct_conn(cli);
+    }
+
+    async fn retain_as_published_test(
+        pub_topic: String,
+        sub_topic: String,
+        retain_as_published: bool,
+        payload_flag: String,
+    ) {
+        let client_id = unique_id();
+        let addr = broker_addr();
+        let sub_topics = &[sub_topic.clone()];
+        let sub_qos = &[QOS_0];
+        let sub_opts = &[SubscribeOptions::new(false, retain_as_published, None)];
+
+        let cli = connect_server5(&client_id, &addr, false, false);
+
+        let message_content = format!("mqtt {payload_flag} message");
+        // publish
+        let msg = Message::new_retained(pub_topic.clone(), message_content.clone(), QOS_0);
+        assert!(cli.publish(msg).is_ok());
+
+        // subscribe
+        let rx = cli.start_consuming();
+        assert!(cli
+            .subscribe_many_with_options(sub_topics, sub_qos, sub_opts, None)
+            .is_ok());
+        let recv = rx.recv_timeout(Duration::from_secs(1));
+        assert!(recv.is_ok());
+        assert_eq!(recv.unwrap().unwrap().retained(), retain_as_published);
+        distinct_conn(cli);
+    }
+
+    async fn retain_handling_test(
+        pub_topic: String,
+        sub_topic: String,
+        retain_handling: RetainHandling,
+        payload_flag: String,
+    ) {
+        let client_id = unique_id();
+
+        println!("client_id:{:#?},topic:{:#?}", client_id, sub_topic.clone());
+
+        let addr = broker_addr();
+        let sub_topics = &[sub_topic.clone()];
+        let sub_qos = &[QOS_0];
+        let sub_opts = &[SubscribeOptions::new(true, false, retain_handling)];
+
+        let cli = connect_server5(&client_id, &addr, false, false);
+
+        let message_content = format!("mqtt {payload_flag} message");
+        // publish
+        let msg = Message::new_retained(pub_topic.clone(), message_content.clone(), QOS_0);
+        assert!(cli.publish(msg.clone()).is_ok());
+
+        // subscribe
+        let consumer_client_id = unique_id();
+        let consumer_cli = connect_server5(&consumer_client_id, &addr, false, false);
+        let mut rx = consumer_cli.start_consuming();
+        assert!(consumer_cli
+            .subscribe_many_with_options(sub_topics, sub_qos, sub_opts, None)
+            .is_ok());
+
+        match retain_handling {
+            RetainHandling::SendRetainedOnSubscribe => {
+                assert!(rx.recv_timeout(Duration::from_secs(1)).is_ok());
+                assert!(rx.recv_timeout(Duration::from_secs(1)).is_ok());
+                let _ = consumer_cli.unsubscribe_many(sub_topics);
+                assert!(consumer_cli
+                    .subscribe_many_with_options(sub_topics, sub_qos, sub_opts, None)
+                    .is_ok());
+                assert!(rx.recv_timeout(Duration::from_secs(1)).is_ok());
+                assert!(rx.recv_timeout(Duration::from_secs(1)).is_err());
+            }
+            RetainHandling::SendRetainedOnNew => {
+                assert!(rx.recv_timeout(Duration::from_secs(1)).is_ok());
+                assert!(rx.recv_timeout(Duration::from_secs(1)).is_ok());
+                let _ = consumer_cli.unsubscribe_many(sub_topics);
+                assert!(consumer_cli
+                    .subscribe_many_with_options(sub_topics, sub_qos, sub_opts, None)
+                    .is_ok());
+                assert!(rx.recv_timeout(Duration::from_secs(1)).is_err());
+            }
+            RetainHandling::DontSendRetained => {
+                assert!(rx.recv_timeout(Duration::from_secs(1)).is_ok());
+                assert!(rx.recv_timeout(Duration::from_secs(1)).is_err());
             }
         }
-
-        if let Some(msg) = rx.iter().next() {
-            let msg = msg.unwrap();
-            let payload = String::from_utf8(msg.payload().to_vec()).unwrap();
-            assert_eq!(payload, message_content);
-        }
+        distinct_conn(consumer_cli);
         distinct_conn(cli);
     }
 }
