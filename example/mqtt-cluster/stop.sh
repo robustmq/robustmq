ac1b38523c5f6e1b4ff54e22f2d3858f22d75a30
Lobo Xu <317307889@qq.com>
2024-11-03 03:48:17 -0600

    test: Fix integration test errors (#589)
    * Journal Engine Segment storage module development

--- example/mqtt-cluster/stop.sh
+++ example/mqtt-cluster/stop.sh
@@ -14,37 +14,37 @@
 
 stop_pc_cluster(){
     no1=`ps -ef | grep placement-center  | grep node-1 | grep -v grep | awk '{print $2}'`
-    if [[ -n $no1 ]]
+    if [ -n "$no1" ]
     then
         echo "kill placement center $no1"
         kill $no1
     fi
 
-    no2=`ps -ef | grep placement-center  | grep node-2 | grep -v grep | awk '{print $2}'`
-    if [[ -n $no2 ]]
-    then
-        echo "kill placement center $no2"
-        kill $no2
-    fi
+    # no2=`ps -ef | grep placement-center  | grep node-2 | grep -v grep | awk '{print $2}'`
+    # if [[ -n $no2 ]]
+    # then
+    #     echo "kill placement center $no2"
+    #     kill $no2
+    # fi
 
-    no3=`ps -ef | grep placement-center  | grep node-3 | grep -v grep | awk '{print $2}'`
-    if [[ -n $no3 ]]
-    then
-        echo "kill placement center $no3"
-        kill $no3
-    fi
+    # no3=`ps -ef | grep placement-center  | grep node-3 | grep -v grep | awk '{print $2}'`
+    # if [[ -n $no3 ]]
+    # then
+    #     echo "kill placement center $no3"
+    #     kill $no3
+    # fi
 
 
     sleep 3
 
     rm -rf  /tmp/robust/placement-center-1/data
-    rm -rf  /tmp/robust/placement-center-2/data
-    rm -rf  /tmp/robust/placement-center-3/data
+    # rm -rf  /tmp/robust/placement-center-2/data
+    # rm -rf  /tmp/robust/placement-center-3/data
 }
 
 stop_mqtt_cluster(){
     no1=`ps -ef | grep mqtt-server  | grep node-1 | grep -v grep | awk '{print $2}'`
-    if [[ -n $no1 ]]
+    if [ -n "$no1" ]
     then
         echo "kill mqtt-server $no1"
         kill $no1
@@ -53,7 +53,7 @@ stop_mqtt_cluster(){
 
 stop_journal_server(){
     no1=`ps -ef | grep journal-server  | grep node-1 | grep -v grep | awk '{print $2}'`
-    if [[ -n $no1 ]]
+    if [ -n "$no1" ]
     then
         echo "kill journal-server $no1"
         kill $no1
