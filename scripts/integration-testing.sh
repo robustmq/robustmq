184d5ef34ebc1c93bdd0010bfee9d060b05d79a1
Lobo Xu <loboxu.1227@bytedance.com>
2024-11-08 00:32:24 +0800

    dev

--- scripts/integration-testing.sh
+++ scripts/integration-testing.sh
@@ -12,6 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 rm -rf /home/runner/work/robustmq/robustmq/target
 
 sh example/mqtt-cluster/start.sh
