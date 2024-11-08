67713bde70d1517713115ed1795511ffab4e7441
porterzhang2021 <porterzhang2021@outlook.com>
2024-11-08 09:16:45 +0800

    test: add more test case for the method called "register_node_method"

--- src/common/base/src/error/common.rs
+++ src/common/base/src/error/common.rs
@@ -78,8 +78,14 @@ pub enum CommonError {
     #[error("Parameter cannot be empty, parameter name: {0}")]
     ParameterCannotBeNull(String),
 
+    #[error("Invalid parameter format, parameter name: {0}")]
+    InvalidParameterFormat(String),
+
     #[error("Module {0} does not support this feature {1}")]
     NotSupportFeature(String, String),
+
+    #[error("Unavailable cluster type")]
+    UnavailableClusterType,
 
     #[error("Unavailable storage type")]
     UnavailableStorageType,
--- src/placement-center/tests/grpc_clients_test.rs
+++ src/placement-center/tests/grpc_clients_test.rs
@@ -17,13 +17,12 @@ mod common;
 mod tests {
     use common_base::error::common::CommonError;
     use protocol::placement_center::placement_center_inner::placement_center_service_client::PlacementCenterServiceClient;
-    use protocol::placement_center::placement_center_inner::{
-        HeartbeatRequest, RegisterNodeRequest, SetIdempotentDataRequest, UnRegisterNodeRequest,
-    };
+    use protocol::placement_center::placement_center_inner::{HeartbeatRequest, RegisterNodeRequest, SetIdempotentDataRequest, UnRegisterNodeRequest};
     use protocol::placement_center::placement_center_journal::engine_service_client::EngineServiceClient;
     use protocol::placement_center::placement_center_journal::{
         CreateNextSegmentRequest, CreateShardRequest, DeleteSegmentRequest, DeleteShardRequest,
     };
+    use tonic::Status;
 
     use crate::common::{
         cluster_name, cluster_type, extend_info, namespace, node_id, node_ip, pc_addr, producer_id,
@@ -31,24 +30,182 @@ mod tests {
     };
 
     #[tokio::test]
-    async fn test_register_node() {
+    async fn test_register_node_method_is_ok() {
+        let mut client = PlacementCenterServiceClient::connect(pc_addr())
+            .await
+            .unwrap();
+        let valid_request = RegisterNodeRequest {
+            cluster_name: cluster_name(),
+            cluster_type: cluster_type(),
+            node_ip: node_ip(),
+            node_id: node_id(),
+            node_inner_addr: node_ip(),
+            extend_info: extend_info(),
+        };
+        let response = client
+            .register_node(tonic::Request::new(valid_request.clone()))
+            .await;
+        assert!(response.is_ok());
+    }
+
+    #[tokio::test]
+    async fn test_register_node_method_param_type_string_is_empty() {
         let mut client = PlacementCenterServiceClient::connect(pc_addr())
             .await
             .unwrap();
 
-        let request = RegisterNodeRequest {
+        let mut valid_request = RegisterNodeRequest {
+            cluster_name: String::new(),
             cluster_type: cluster_type(),
-            cluster_name: cluster_name(),
-            node_id: node_id(),
             node_ip: node_ip(),
+            node_id: node_id(),
+            node_inner_addr: node_ip(),
             extend_info: extend_info(),
-            ..Default::default()
         };
-        client
-            .register_node(tonic::Request::new(request))
+
+        let valid_field = [
+            "cluster_name",
+            "node_ip",
+            "node_inner_addr"
+        ];
+
+        for field in valid_field {
+            match field {
+                "cluster_name" => valid_request.cluster_name = String::new(),
+                "node_ip" => valid_request.node_ip = String::new(),
+                "node_inner_addr" => valid_request.node_inner_addr = String::new(),
+                _ => unreachable!(),
+            }
+
+            let response = client
+                .register_node(tonic::Request::new(valid_request.clone()))
+                .await;
+
+            assert!(response.is_err());
+            let status = response.unwrap_err();
+            assert_eq!(status.code(),
+                       Status::invalid_argument(CommonError::InvalidParameterFormat(field.to_string())
+                           .to_string()).code());
+            assert_eq!(status.message(),
+                       CommonError::InvalidParameterFormat(field.to_string()).to_string()
+            );
+        }
+    }
+
+    #[tokio::test]
+    async fn test_register_node_method_param_ip_is_correct() {
+        let mut client = PlacementCenterServiceClient::connect(pc_addr())
             .await
             .unwrap();
+
+        let mut valid_request = RegisterNodeRequest {
+            cluster_name: cluster_name(),
+            cluster_type: cluster_type(),
+            node_ip: node_ip(),
+            node_id: node_id(),
+            node_inner_addr: node_ip(),
+            extend_info: extend_info(),
+        };
+
+        let valid_ip = [
+            "192.168.1.1",
+            "1.1.1.1",
+            "255.255.255.255",
+            "0.0.0.0",
+            "123.112.154.123",
+            "172.192.140.21"
+        ];
+
+        let field = [
+            "node_ip",
+            "node_inner_addr"
+        ];
+
+        let mut field_ip: Vec<(&str, &str)> = Vec::new();
+
+        for field in field {
+            for ip in valid_ip {
+                field_ip.push((field, ip))
+            }
+        }
+
+        for (field, ip) in field_ip {
+            match field {
+                "node_ip" => valid_request.node_ip = ip.parse().unwrap(),
+                "node_inner_addr" => valid_request.node_inner_addr = ip.parse().unwrap(),
+                _ => unreachable!(),
+            }
+
+            let response = client
+                .register_node(tonic::Request::new(valid_request.clone()))
+                .await;
+
+            assert!(response.is_ok());
+        }
     }
+
+    #[tokio::test]
+    async fn test_register_node_method_param_ip_is_err() {
+        let mut client = PlacementCenterServiceClient::connect(pc_addr())
+            .await
+            .unwrap();
+
+        let mut valid_request = RegisterNodeRequest {
+            cluster_name: cluster_name(),
+            cluster_type: cluster_type(),
+            node_ip: node_ip(),
+            node_id: node_id(),
+            node_inner_addr: node_ip(),
+            extend_info: extend_info(),
+        };
+
+        let valid_ip = [
+            "255",
+            "255.255",
+            "255.255.255",
+            "255.255.255.255",
+            "256.256.256.256",
+            "256.111.111.111",
+            "111.256.111.111",
+            "111.111.256.111",
+            "111.111.111.256"
+        ];
+
+        let field = [
+            "node_ip",
+            "node_inner_addr"
+        ];
+
+        let mut field_ip: Vec<(&str, &str)> = Vec::new();
+
+        for field in field {
+            for ip in valid_ip {
+                field_ip.push((field, ip))
+            }
+        }
+
+        for (field, ip) in field_ip {
+            match field {
+                "node_ip" => valid_request.node_ip = ip.parse().unwrap(),
+                "node_inner_addr" => valid_request.node_inner_addr = ip.parse().unwrap(),
+                _ => unreachable!(),
+            }
+
+            let response = client
+                .register_node(tonic::Request::new(valid_request.clone()))
+                .await;
+            assert!(response.is_err());
+            if let Err(ref e) = response {
+                assert_eq!(e.code(), tonic::Code::InvalidArgument);
+                assert_eq!(
+                    e.message(),
+                    CommonError::InvalidParameterFormat(field.to_string())
+                        .to_string()
+                );
+            }
+        }
+    }
+
 
     #[tokio::test]
     async fn test_heartbeat() {
