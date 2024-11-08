5b5402fcfc85c45a29bcf6c28ecfc24d9de65c0e
PorterZhang2021 <PorterZhang2021@outlook.com>
2024-11-08 17:50:52 +0800

    Refactor: Add validation for RegisterNodeRequest and supplement related tests.

--- src/common/base/src/error/common.rs
+++ src/common/base/src/error/common.rs
@@ -78,8 +78,8 @@ pub enum CommonError {
     #[error("Parameter cannot be empty, parameter name: {0}")]
     ParameterCannotBeNull(String),
 
-    #[error("Invalid parameter format, parameter name: {0}")]
-    InvalidParameterFormat(String),
+    #[error("Invalid parameter format, parameter name: {0}, parameter_value: {1}")]
+    InvalidParameterFormat(String, String),
 
     #[error("Module {0} does not support this feature {1}")]
     NotSupportFeature(String, String),
