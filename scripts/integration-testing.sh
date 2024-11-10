959721677d0a6257d910395e1330297e3ccaf7f2
Lobo Xu <loboxu.1227@bytedance.com>
2024-11-10 09:52:48 +0800

    dev

--- scripts/integration-testing.sh
+++ scripts/integration-testing.sh
@@ -12,10 +12,11 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 rm -rf /tmp/robust-test
 rm -rf build/
 make build
-
+rm -rf target
 start_server(){
     cd build
     tar -xzvf robustmq-local.tar.gz
