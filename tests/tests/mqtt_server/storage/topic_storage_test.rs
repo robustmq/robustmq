32b4293d7480862573948713289b85260a0068d0
HonokaKousaka <lightofspeedwork@gmail.com>
2024-11-24 22:58:04 -0500

    Add license and reformat the code

--- tests/tests/mqtt_server/storage/topic_storage_test.rs
+++ tests/tests/mqtt_server/storage/topic_storage_test.rs
@@ -1,4 +1,4 @@
-// Copyright 2024 RobustMQ Team
+// Copyright 2023 RobustMQ Team
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -23,16 +23,12 @@ mod tests {
     use grpc_clients::pool::ClientPool;
     use metadata_struct::mqtt::message::MqttMessage;
     use metadata_struct::mqtt::topic::MqttTopic;
-    use protocol::mqtt::common::{Publish, PublishProperties};
-
     use mqtt_broker::storage::topic::TopicStorage;
+    use protocol::mqtt::common::{Publish, PublishProperties};
 
     #[tokio::test]
     async fn topic_test() {
-        let path = format!(
-            "{}/../config/mqtt-server.toml",
-            env!("CARGO_MANIFEST_DIR")
-        );
+        let path = format!("{}/../config/mqtt-server.toml", env!("CARGO_MANIFEST_DIR"));
         let log_config = format!("{}/../config/log4rs.yaml", env!("CARGO_MANIFEST_DIR"));
         let log_path = format!("{}/../logs/tests", env!("CARGO_MANIFEST_DIR"));
 
@@ -42,10 +38,14 @@ mod tests {
         let client_pool: Arc<ClientPool> = Arc::new(ClientPool::new(10));
         let topic_storage = TopicStorage::new(client_pool);
 
-        let mqtt_conf= broker_mqtt_conf();
+        let mqtt_conf = broker_mqtt_conf();
 
         let topic_name: String = "test_password".to_string();
-        let topic = MqttTopic::new(unique_id(), mqtt_conf.cluster_name.clone(), topic_name.clone());
+        let topic = MqttTopic::new(
+            unique_id(),
+            mqtt_conf.cluster_name.clone(),
+            topic_name.clone(),
+        );
         topic_storage.save_topic(topic).await.unwrap();
 
         let result = topic_storage
@@ -73,10 +73,7 @@ mod tests {
 
     #[tokio::test]
     async fn topic_retain_message_test() {
-        let path = format!(
-            "{}/../config/mqtt-server.toml",
-            env!("CARGO_MANIFEST_DIR")
-        );
+        let path = format!("{}/../config/mqtt-server.toml", env!("CARGO_MANIFEST_DIR"));
 
         init_broker_mqtt_conf_by_path(&path);
 
@@ -94,7 +91,11 @@ mod tests {
 
         let mqtt_conf = broker_mqtt_conf();
 
-        let topic = MqttTopic::new(unique_id(), mqtt_conf.cluster_name.clone(), topic_name.clone());
+        let topic = MqttTopic::new(
+            unique_id(),
+            mqtt_conf.cluster_name.clone(),
+            topic_name.clone(),
+        );
         topic_storage.save_topic(topic).await.unwrap();
 
         let result_message = topic_storage
