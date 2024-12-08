75e4e74daa146c85cb3c881f150140b7d5da370e
MangerLe <51878514+MangerLe@users.noreply.github.com>
2024-12-08 15:25:28 +0800

    feat(http): add raft metrics endpoint- Implement new HTTP endpoint for Raft metrics (#684)
    - Add raft_metrics function to collect and format Raft metrics data- Update routes to include new RAFT_METRICS endpoint

--- src/placement-center/src/server/http/index.rs
+++ src/placement-center/src/server/http/index.rs
@@ -12,7 +12,21 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use axum::extract::State;
 use common_base::metrics::dump_metrics;
+
+use crate::server::http::server::HttpServerState;
+
 pub async fn metrics() -> String {
     dump_metrics()
+}
+
+pub async fn raft_metrics(state: State<HttpServerState>) -> String {
+    let metrics = state
+        .placement_center_storage
+        .openraft_node
+        .metrics()
+        .borrow()
+        .clone();
+    serde_json::to_string(&metrics).unwrap()
 }
