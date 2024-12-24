728c935bef2ed32ee1f308b3dcd2b3d2ebbb018a
porterzhang2021 <porterzhang2021@outlook.com>
2024-12-24 20:46:25 +0800

    chore: fix ci error

--- scripts/journal-ig-test.sh
+++ scripts/journal-ig-test.sh
@@ -59,13 +59,24 @@ rm -rf ./robust-data-test/journal-server*
 start_placement_server
 start_journal_server
 
-cargo nextest run --profile ci --package grpc-clients --test mod -- journal
-cargo nextest run --profile ci --package robustmq-test --test mod -- journal_client
-cargo nextest run --profile ci --package robustmq-test --test mod -- journal_server
+
 
 
 if [ "$1" = "dev" ]; then
+
+  cargo nextest run  --package grpc-clients --test mod -- journal
+  cargo nextest run  --package robustmq-test --test mod -- journal_client
+  cargo nextest run  --package robustmq-test --test mod -- journal_server
+
+
 # Stop Server
 stop_placement_server
 stop_journal_server
+
+else
+
+  cargo nextest run --profile ci --package grpc-clients --test mod -- journal
+  cargo nextest run --profile ci --package robustmq-test --test mod -- journal_client
+  cargo nextest run --profile ci --package robustmq-test --test mod -- journal_server
+
 fi
