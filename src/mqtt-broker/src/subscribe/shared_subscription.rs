9c27292c370b7011c77e1eae2a658a8da66c6723
PorterZhang2021 <PorterZhang2021@outlook.com>
2025-02-13 18:03:38 +0800

    refactor: add shared_subscription

--- src/mqtt-broker/src/subscribe/shared_subscription.rs
+++ src/mqtt-broker/src/subscribe/shared_subscription.rs
@@ -12,11 +12,12 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#[allow(dead_code)]
 enum SubscriptionMode {
     Shared { group_name: String },
     Queue,
 }
-
+#[allow(dead_code)]
 struct SharedSubscription {
     subscription_type: SubscriptionMode,
     topic_pattern: String,
