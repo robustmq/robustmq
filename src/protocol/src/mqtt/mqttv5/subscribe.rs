faaf67e5ba449ffc286fc441c07d7d5bd54b8c95
Yikrr8 <790556471@qq.com>
2025-02-21 18:10:43 +0800

    dev

--- src/protocol/src/mqtt/mqttv5/subscribe.rs
+++ src/protocol/src/mqtt/mqttv5/subscribe.rs
@@ -45,10 +45,10 @@ pub fn read(
         let options = read_u8(&mut bytes)?;
         let requested_qos = options & 0b0000_0011;
 
-        let nolocal = options >> 2 & 0b0000_0001;
+        let nolocal = (options >> 2) & 0b0000_0001;
         let nolocal = nolocal != 0;
 
-        let preserve_retain = options >> 3 & 0b0000_0001;
+        let preserve_retain = (options >> 3) & 0b0000_0001;
         let preserve_retain = preserve_retain != 0;
 
         let retain_forward_rule = (options >> 4) & 0b0000_0011;
