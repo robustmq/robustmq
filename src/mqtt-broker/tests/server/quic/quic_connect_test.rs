8f58c03bbb53f47fbbbb58ce3a890e75a41fff82
PorterZhang2021 <PorterZhang2021@outlook.com>
2025-02-24 17:21:16 +0800

    test: client send packet to server

--- src/mqtt-broker/tests/server/quic/quic_connect_test.rs
+++ src/mqtt-broker/tests/server/quic/quic_connect_test.rs
@@ -17,10 +17,11 @@ mod tests {
     use bytes::BytesMut;
     use mqtt_broker::server::quic::client::QuicClient;
     use mqtt_broker::server::quic::server::QuicServer;
-    use protocol::mqtt::codec::MqttCodec;
+    use protocol::mqtt::codec::{MqttCodec, MqttPacketWrapper};
+    use robustmq_test::mqtt_build_tool::build_connect::build_mqtt4_pg_connect;
     use std::net::{IpAddr, Ipv4Addr, SocketAddr};
     use std::sync::Arc;
-    use tokio_util::codec::Encoder;
+    use tokio_util::codec::{Decoder, Encoder};
 
     #[tokio::test]
     async fn quic_client_should_connect_quic_server() {
@@ -247,9 +248,97 @@ mod tests {
 
     #[tokio::test]
     async fn try_encode_data_from_mqtt_encoder() {
-        const MSG: &[u8; 5] = b"hello";
-        let bytes_mut = BytesMut::with_capacity(MSG.len());
-        let mqtt_codec = MqttCodec::new(None);
-        mqtt_codec.encode()
+        let mut mqtt_codec = MqttCodec::new(None);
+        let connect = build_mqtt4_pg_connect();
+        let mqtt_packet_wrapper = MqttPacketWrapper {
+            protocol_version: 4,
+            packet: connect,
+        };
+        let mut bytes_mut = BytesMut::with_capacity(0);
+        mqtt_codec
+            .encode(mqtt_packet_wrapper, &mut bytes_mut)
+            .unwrap();
+        assert!(bytes_mut.len() > 0);
+    }
+
+    #[tokio::test]
+    async fn try_decode_data_from_mqtt_decoder() {
+        let mut mqtt_codec = MqttCodec::new(None);
+        let connect = build_mqtt4_pg_connect();
+        let mqtt_packet_wrapper = MqttPacketWrapper {
+            protocol_version: 4,
+            packet: connect,
+        };
+        let mut bytes_mut = BytesMut::with_capacity(0);
+        mqtt_codec
+            .encode(mqtt_packet_wrapper, &mut bytes_mut)
+            .unwrap();
+        assert!(bytes_mut.len() > 0);
+        let packet = mqtt_codec.decode(&mut bytes_mut).unwrap();
+        assert!(packet.is_some());
+    }
+
+    #[tokio::test]
+    async fn client_should_send_packet_to_server() {
+        let ip_server: SocketAddr = SocketAddr::new(IpAddr::V4(Ipv4Addr::LOCALHOST), 0);
+
+        let mut server = QuicServer::new(ip_server);
+        server.start();
+
+        let ip_server_addr = server.local_addr();
+
+        let client_addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::LOCALHOST), 0);
+
+        let mut quic_client = QuicClient::bind(client_addr);
+
+        let client_addr = quic_client.local_addr();
+
+        let mut mqtt_codec = MqttCodec::new(None);
+        let connect = build_mqtt4_pg_connect();
+        let mqtt_packet_wrapper = MqttPacketWrapper {
+            protocol_version: 4,
+            packet: connect,
+        };
+
+        let mut bytes_mut = BytesMut::with_capacity(0);
+        mqtt_codec
+            .encode(mqtt_packet_wrapper, &mut bytes_mut)
+            .unwrap();
+
+        let write_send = Arc::new(tokio::sync::Notify::new());
+        let write_recv = write_send.clone();
+
+        let mut validate_bytes_mute = bytes_mut.clone();
+
+        let server = tokio::spawn(async move {
+            let endpoint = server.get_endpoint().unwrap();
+            let incoming_conn = endpoint.accept().await.unwrap();
+            let conn = incoming_conn.await.unwrap();
+            assert_eq!(conn.remote_address(), client_addr);
+            write_recv.notified().await;
+            let (_send_stream, mut recv_stream) = conn.accept_bi().await.unwrap();
+            let mut decode_bytes = BytesMut::with_capacity(0);
+
+            let vec = recv_stream.read_to_end(1024).await.unwrap();
+
+            decode_bytes.extend(vec);
+            assert_eq!(validate_bytes_mute.len(), decode_bytes.len());
+
+            let packet = mqtt_codec.decode(&mut decode_bytes).unwrap();
+            assert!(packet.is_some());
+        });
+
+        let connection = quic_client
+            .connect(ip_server_addr, "localhost")
+            .await
+            .unwrap();
+        let (mut send_stream, _recv_stream) = connection.open_bi().await.unwrap();
+
+        send_stream.write_all(bytes_mut.as_mut()).await.unwrap();
+        send_stream.finish().unwrap();
+        let _ = send_stream.stopped().await;
+        write_send.notify_one();
+
+        server.await.unwrap();
     }
 }
