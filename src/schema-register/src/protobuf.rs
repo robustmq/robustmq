30d25d334674b65c9b96996a5dddbdcaebc988da
Yu Liu <60283975+LYZJU2019@users.noreply.github.com>
2025-03-04 19:15:00 -0600

    feat: add validation for protobuf schema (#902)

--- src/schema-register/src/protobuf.rs
+++ src/schema-register/src/protobuf.rs
@@ -13,65 +13,58 @@
 // limitations under the License.
 
 use common_base::error::common::CommonError;
+use metadata_struct::schema::SchemaData;
+use protofish::{decode::Value, prelude::Context};
 
-pub fn protobuf_validate() -> Result<bool, CommonError> {
+pub fn protobuf_validate(
+    schema_data: &SchemaData,
+    data: &[u8],
+    message_name: &str,
+) -> Result<bool, CommonError> {
+    let context = Context::parse([schema_data.schema.as_str()]).map_err(|err| {
+        CommonError::CommonError(format!(
+            "Failed to parse schema {}: {}",
+            schema_data.name.as_str(),
+            err
+        ))
+    })?;
+
+    let message = context.get_message(message_name).ok_or_else(|| {
+        CommonError::CommonError(format!(
+            "Message {} not found in schema {}",
+            message_name,
+            schema_data.name.as_str()
+        ))
+    })?;
+
+    let decoded = message.decode(data, &context);
+
+    // Check if there are any unknown or incomplete fields
+    for field in decoded.fields {
+        match &field.value {
+            Value::Unknown(_) | Value::Incomplete(_, _) => {
+                return Ok(false); // Invalid if unknown or incomplete fields exist
+            }
+            _ => {} // Otherwise, it's fine
+        }
+    }
+
     Ok(true)
 }
 
 #[cfg(test)]
 mod test {
     use crate::protobuf::protobuf_validate;
-    // use protobuf::descriptor::FileDescriptorProto;
-    // use protobuf::reflect::FileDescriptor;
-    // use protobuf::text_format::parse_from_str;
-    use bytes::Bytes;
-    use protofish::decode::UnknownValue;
-    use protofish::prelude::*;
+    use metadata_struct::schema::{SchemaData, SchemaType};
 
     #[test]
     pub fn protobuf_validate_test() {
-        let result = protobuf_validate();
-        println!("{:?}", result);
-        assert!(result.is_ok());
+        protofish_example_test();
+        protofish_schema_test();
     }
 
-    #[test]
-    pub fn protobuf_test() {
-        // 动态提供的 Protobuf 定义字符串
-        // let proto_content = r#"
-        //     syntax = "proto3";
-        //     package mypackage;
-
-        //     message Person {
-        //         string name = 1;
-        //         uint32 age = 2;
-        //     }
-        // "#;
-
-        // // 将 Protobuf 定义字符串解析为 FileDescriptorProto
-        // let file_descriptor_proto = parse_from_str::<FileDescriptorProto>(proto_content).unwrap();
-        // println!("{:?}", file_descriptor_proto);
-        // // // 创建 FileDescriptor
-        // let file_descriptor = FileDescriptor::new_dynamic(file_descriptor_proto, None).unwrap();
-
-        // // 获取消息类型描述符
-        // let message_descriptor = file_descriptor
-        //     .message_by_full_name("mypackage.Person")
-        //     .unwrap();
-
-        // // 待校验的二进制数据
-        // let data = vec![...]; // 替换为你的二进制数据
-
-        // // 尝试将数据反序列化为 DynamicMessage
-        // let dynamic_message = DynamicMessage::decode(message_descriptor, &data)?;
-
-        // println!("数据符合 Protobuf 定义");
-        // println!("内容: {:?}", dynamic_message);
-    }
-
-    #[test]
     pub fn protofish_example_test() {
-        let context = Context::parse([r#"
+        let schema = r#"
             syntax = "proto3";
             package Proto;
 
@@ -80,40 +73,35 @@ mod test {
             service Fish {
                 rpc Swim( Request ) returns ( Response );
             }
-        "#])
-        .unwrap();
+        "#;
 
-        let service = context.get_service("Proto.Fish").unwrap();
-        let rpc = service.rpc_by_name("Swim").unwrap();
+        let schema_data = SchemaData {
+            cluster_name: "test_cluster".to_string(),
+            name: "Proto".to_string(),
+            schema_type: SchemaType::PROTOBUF,
+            desc: "".to_string(),
+            schema: schema.to_string(),
+        };
 
-        let input = context.decode(rpc.input.message, b"\x0a\x05Perch");
-        assert_eq!(input.fields[0].number, 1);
-        assert_eq!(input.fields[0].value, Value::String(String::from("Perch")));
+        let res = protobuf_validate(&schema_data, b"\x0a\x05Perch", "Proto.Request");
+        assert!(res.is_ok());
+        assert!(res.unwrap());
 
-        let output = context.decode(rpc.output.message, b"\x08\xa9\x46");
-        assert_eq!(output.fields[0].number, 1);
-        assert_eq!(output.fields[0].value, Value::Int32(9001));
+        let res = protobuf_validate(&schema_data, b"\x08\xa9\x46", "Proto.Response");
+        assert!(res.is_ok());
+        assert!(res.unwrap());
 
-        let bytes = b"\x12\x07Unknown\x0a\x0fAtlantic ";
-        let request = context.get_message("Proto.Request").unwrap();
-        let value = request.decode(bytes, &context);
-        assert_eq!(value.fields[0].number, 2);
-        assert_eq!(
-            value.fields[0].value,
-            Value::Unknown(UnknownValue::VariableLength(Bytes::from_static(b"Unknown")))
+        let res = protobuf_validate(
+            &schema_data,
+            b"\x12\x07Unknown\x0a\x0fAtlantic ",
+            "Proto.Request",
         );
-        assert_eq!(
-            value.fields[1].value,
-            Value::Incomplete(2, Bytes::from_static(b"\x0fAtlantic "))
-        );
-
-        let encoded = value.encode(&context);
-        assert_eq!(encoded, &bytes[..]);
+        assert!(res.is_ok());
+        assert!(!res.unwrap());
     }
 
-    #[test]
     pub fn protofish_schema_test() {
-        let context = Context::parse([r#"
+        let schema = r#"
             syntax = "proto3";
             package MyPackage;
 
@@ -130,8 +118,15 @@ mod test {
                 uint32 age = 2;
                 repeated Experience experience = 3;
             }
-        "#])
-        .unwrap();
+        "#;
+
+        let schema_data = SchemaData {
+            cluster_name: "test_cluster".to_string(),
+            name: "MyPackage".to_string(),
+            schema_type: SchemaType::PROTOBUF,
+            desc: "".to_string(),
+            schema: schema.to_string(),
+        };
 
         // ----- Experience -----
         // Experience {
@@ -141,51 +136,14 @@ mod test {
         //     end_year: 2023,
         //     skills: ["Java", "Google Cloud", "Software Testing"]
         // }
-        let experience_raw = b"\x0a\x06Google\x12\x11Software Engineer\x18\xe5\x0f\x20\xe7\x0f\x2a\x04Java\x2a\x0cGoogle Cloud\x2a\x10Software Testing";
-
-        let experience_message = context.get_message("MyPackage.Experience").unwrap();
-        let experience_value = experience_message.decode(experience_raw, &context);
-
-        // company
-        assert_eq!(experience_value.fields[0].number, 1);
-        assert_eq!(
-            experience_value.fields[0].value,
-            Value::String(String::from("Google"))
+        let res = protobuf_validate(
+            &schema_data,
+            b"\x0a\x06Google\x12\x11Software Engineer\x18\xe5\x0f\x20\xe7\x0f\x2a\x04Java\x2a\x0cGoogle Cloud\x2a\x10Software Testing",
+            "MyPackage.Experience"
         );
 
-        // position
-        assert_eq!(experience_value.fields[1].number, 2);
-        assert_eq!(
-            experience_value.fields[1].value,
-            Value::String(String::from("Software Engineer"))
-        );
-
-        // start_year
-        assert_eq!(experience_value.fields[2].number, 3);
-        assert_eq!(experience_value.fields[2].value, Value::UInt32(2021));
-
-        // end_year
-        assert_eq!(experience_value.fields[3].number, 4);
-        assert_eq!(experience_value.fields[3].value, Value::UInt32(2023));
-
-        // skills, currently protofish does not support repeated strings
-        assert_eq!(experience_value.fields[4].number, 5);
-        assert_eq!(
-            experience_value.fields[4].value,
-            Value::String(String::from("Java"))
-        );
-
-        assert_eq!(experience_value.fields[5].number, 5);
-        assert_eq!(
-            experience_value.fields[5].value,
-            Value::String(String::from("Google Cloud"))
-        );
-
-        assert_eq!(experience_value.fields[6].number, 5);
-        assert_eq!(
-            experience_value.fields[6].value,
-            Value::String(String::from("Software Testing"))
-        );
+        assert!(res.is_ok());
+        assert!(res.unwrap());
 
         // ----- Person -----
         // Person {
@@ -193,27 +151,13 @@ mod test {
         //     age: 30,
         //     experience: [experience]
         // }
-        let person_raw = b"\x0a\x04John\x10\x1e\x1a\x47\x0a\x06Google\x12\x11Software Engineer\x18\xe5\x0f\x20\xe7\x0f\x2a\x04Java\x2a\x0cGoogle Cloud\x2a\x10Software Testing";
-
-        let person_message = context.get_message("MyPackage.Person").unwrap();
-        let person_value = person_message.decode(person_raw, &context);
-
-        // name
-        assert_eq!(person_value.fields[0].number, 1);
-        assert_eq!(
-            person_value.fields[0].value,
-            Value::String(String::from("John"))
+        let res = protobuf_validate(
+            &schema_data,
+            b"\x0a\x04John\x10\x1e\x1a\x47\x0a\x06Google\x12\x11Software Engineer\x18\xe5\x0f\x20\xe7\x0f\x2a\x04Java\x2a\x0cGoogle Cloud\x2a\x10Software Testing",
+            "MyPackage.Person"
         );
 
-        // age
-        assert_eq!(person_value.fields[1].number, 2);
-        assert_eq!(person_value.fields[1].value, Value::UInt32(30));
-
-        // experience
-        assert_eq!(person_value.fields[2].number, 3);
-        assert_eq!(
-            person_value.fields[2].value,
-            Value::Message(Box::new(experience_value))
-        );
+        assert!(res.is_ok());
+        assert!(res.unwrap());
     }
 }
