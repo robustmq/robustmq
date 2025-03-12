54b42c53009df3d2aa1086641c3e0b60173121e8
Yikrr8 <790556471@qq.com>
2025-03-12 14:21:31 +0800

    dev

--- src/placement-center/src/server/grpc/intercept.rs
+++ src/placement-center/src/server/grpc/intercept.rs
@@ -1,9 +1,6 @@
 use tonic::{Request, Status};
 
 pub fn grpc_intercept(req: Request<()>) -> Result<Request<()>, Status> {
-    println!("Intercepting request metadata: {:?}", req.metadata());
-    println!("Intercepting request remote_addr: {:?}", req.remote_addr());
-    println!("Intercepting request extensions: {:?}", req.extensions());
 
     Ok(req)
 }
