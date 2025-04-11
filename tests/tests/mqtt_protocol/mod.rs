64d515da63dcd5f5c810784f7ddcba5c3a896aa2
alex <alex@gmail.com>
2025-04-11 11:20:50 +0800

    dev

--- tests/tests/mqtt_protocol/mod.rs
+++ tests/tests/mqtt_protocol/mod.rs
@@ -30,12 +30,12 @@ pub struct ClientTestProperties {
 }
 
 pub mod acl_test;
+pub mod auth_test;
 pub mod common;
 pub mod connect5_test;
 pub mod connect_packet_size_test;
 pub mod connect_test;
-// mod flapping_detect_test;
-pub mod auth_test;
+mod flapping_detect_test;
 pub mod keep_alive_test;
 pub mod lastwill_message_test;
 pub mod qos_test;
