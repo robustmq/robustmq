dce4078d05542e9c44a61fb7c3cd5680bcb0ecde
PorterZhang2021 <porterzhang2021@outlook.com>
2025-05-02 17:55:37 +0800

    refactor: refactor mqtt broker

--- src/mqtt-broker/src/observability/system_topic/stats/mod.rs
+++ src/mqtt-broker/src/observability/system_topic/stats/mod.rs
@@ -37,3 +37,221 @@ pub const SYSTEM_TOPIC_BROKERS_STATS_TOPICS_MAX: &str = "$SYS/brokers/${node}/st
 // routes
 pub const SYSTEM_TOPIC_BROKERS_STATS_ROUTES_COUNT: &str = "$SYS/brokers/${node}/stats/routes/count";
 pub const SYSTEM_TOPIC_BROKERS_STATS_ROUTES_MAX: &str = "$SYS/brokers/${node}/stats/routes/max";
+
+/*
+pub(crate) async fn report_broker_stat_connections_count<S>(
+    client_pool: &Arc<ClientPool>,
+    metadata_cache: &Arc<CacheManager>,
+    message_storage_adapter: &Arc<S>,
+) where
+    S: StorageAdapter + Clone + Send + Sync + 'static,
+{
+    report_system_data(
+        client_pool,
+        metadata_cache,
+        message_storage_adapter,
+        SYSTEM_TOPIC_BROKERS_STATS_CONNECTIONS_COUNT,
+        || async { metadata_cache.get_connection_count().to_string() },
+    )
+    .await;
+}
+
+pub(crate) async fn report_broker_stat_connections_max<S>(
+    client_pool: &Arc<ClientPool>,
+    metadata_cache: &Arc<CacheManager>,
+    message_storage_adapter: &Arc<S>,
+) where
+    S: StorageAdapter + Clone + Send + Sync + 'static,
+{
+    report_system_data(
+        client_pool,
+        metadata_cache,
+        message_storage_adapter,
+        SYSTEM_TOPIC_BROKERS_STATS_CONNECTIONS_MAX,
+        ||  async {
+            metadata_cache.get_connection_max().to_string()
+        },
+    ).await;
+}
+
+
+pub(crate) async fn report_broker_stat_suboptions_count<S>(
+    client_pool: &Arc<ClientPool>,
+    metadata_cache: &Arc<CacheManager>,
+    message_storage_adapter: &Arc<S>,
+) where
+    S: StorageAdapter + Clone + Send + Sync + 'static,
+{
+    report_system_data(
+        client_pool,
+        metadata_cache,
+        message_storage_adapter,
+        SYSTEM_TOPIC_BROKERS_STATS_SUBOPTIONS_COUNT,
+        ||  async {
+            metadata_cache.get_suboptions_count().to_string()
+        },
+    ).await;
+}
+
+pub(crate) async fn report_broker_stat_suboptions_max<S>(
+    client_pool: &Arc<ClientPool>,
+    metadata_cache: &Arc<CacheManager>,
+    message_storage_adapter: &Arc<S>,
+) where
+    S: StorageAdapter + Clone + Send + Sync + 'static,
+{
+    report_system_data(
+        client_pool,
+        metadata_cache,
+        message_storage_adapter,
+        SYSTEM_TOPIC_BROKERS_STATS_SUBOPTIONS_MAX,
+        ||  async {
+            metadata_cache.get_suboptions_max().to_string()
+        },
+    ).await;
+}
+
+pub(crate) async fn report_broker_stat_subscribers_count<S>(
+    client_pool: &Arc<ClientPool>,
+    metadata_cache: &Arc<CacheManager>,
+    message_storage_adapter: &Arc<S>,
+) where
+    S: StorageAdapter + Clone + Send + Sync + 'static,
+{
+    report_system_data(
+        client_pool,
+        metadata_cache,
+        message_storage_adapter,
+        SYSTEM_TOPIC_BROKERS_STATS_SUBSCRIBERS_COUNT,
+        ||  async {
+            metadata_cache.get_subscribers_count().to_string()
+        },
+    ).await;
+}
+
+pub(crate) async fn report_broker_stat_subscriptions_count<S>(
+    client_pool: &Arc<ClientPool>,
+    metadata_cache: &Arc<CacheManager>,
+    message_storage_adapter: &Arc<S>,
+) where
+    S: StorageAdapter + Clone + Send + Sync + 'static,
+{
+    report_system_data(
+        client_pool,
+        metadata_cache,
+        message_storage_adapter,
+        SYSTEM_TOPIC_BROKERS_STATS_SUBSCRIPTIONS_COUNT,
+        ||  async {
+            metadata_cache.get_subscriptions_count().to_string()
+        },
+    ).await;
+}
+
+pub(crate) async fn report_broker_stat_subscriptions_shared_count<S>(
+    client_pool: &Arc<ClientPool>,
+    metadata_cache: &Arc<CacheManager>,
+    message_storage_adapter: &Arc<S>,
+) where
+    S: StorageAdapter + Clone + Send + Sync + 'static,
+{
+    report_system_data(
+        client_pool,
+        metadata_cache,
+        message_storage_adapter,
+        SYSTEM_TOPIC_BROKERS_STATS_SUBSCRIPTIONS_SHARED_COUNT,
+        ||  async {
+            metadata_cache.get_shared_subscriptions_count().to_string()
+        },
+    ).await;
+}
+
+pub(crate) async fn report_broker_stat_subscriptions_shared_max<S>(
+    client_pool: &Arc<ClientPool>,
+    metadata_cache: &Arc<CacheManager>,
+    message_storage_adapter: &Arc<S>,
+) where
+    S: StorageAdapter + Clone + Send + Sync + 'static,
+{
+    report_system_data(
+        client_pool,
+        metadata_cache,
+        message_storage_adapter,
+        SYSTEM_TOPIC_BROKERS_STATS_SUBSCRIPTIONS_SHARED_MAX,
+        ||  async {
+            metadata_cache.get_shared_subscriptions_max().to_string()
+        },
+    ).await;
+}
+
+
+pub(crate) async fn report_broker_stat_topics_count<S>(
+    client_pool: &Arc<ClientPool>,
+    metadata_cache: &Arc<CacheManager>,
+    message_storage_adapter: &Arc<S>,
+) where
+    S: StorageAdapter + Clone + Send + Sync + 'static,
+{
+    report_system_data(
+        client_pool,
+        metadata_cache,
+        message_storage_adapter,
+        SYSTEM_TOPIC_BROKERS_STATS_TOPICS_COUNT,
+        ||  async {
+            metadata_cache.get_topics_count().to_string()
+        },
+    ).await;
+}
+
+pub(crate) async fn report_broker_stat_topics_max<S>(
+    client_pool: &Arc<ClientPool>,
+    metadata_cache: &Arc<CacheManager>,
+    message_storage_adapter: &Arc<S>,
+) where
+    S: StorageAdapter + Clone + Send + Sync + 'static,
+{
+    report_system_data(
+        client_pool,
+        metadata_cache,
+        message_storage_adapter,
+        SYSTEM_TOPIC_BROKERS_STATS_TOPICS_MAX,
+        ||  async {
+            metadata_cache.get_topics_max().to_string()
+        },
+    ).await;
+}
+
+pub(crate) async fn report_broker_stat_routes_count<S>(
+    client_pool: &Arc<ClientPool>,
+    metadata_cache: &Arc<CacheManager>,
+    message_storage_adapter: &Arc<S>,
+) where
+    S: StorageAdapter + Clone + Send + Sync + 'static,
+{
+    report_system_data(
+        client_pool,
+        metadata_cache,
+        message_storage_adapter,
+        SYSTEM_TOPIC_BROKERS_STATS_ROUTES_COUNT,
+        ||  async {
+            metadata_cache.get_routes_count().to_string()
+        },
+    ).await;
+}
+
+pub(crate) async fn report_broker_stat_routes_max<S>(
+    client_pool: &Arc<ClientPool>,
+    metadata_cache: &Arc<CacheManager>,
+    message_storage_adapter: &Arc<S>,
+) where
+    S: StorageAdapter + Clone + Send + Sync + 'static,
+{
+    report_system_data(
+        client_pool,
+        metadata_cache,
+        message_storage_adapter,
+        SYSTEM_TOPIC_BROKERS_STATS_ROUTES_MAX,
+        ||  async {
+            metadata_cache.get_routes_max().to_string()
+        },
+    ).await;
+}*/
