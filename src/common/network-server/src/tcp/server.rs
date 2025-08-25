d7711d6886d8880ec5e10a9e63a85ae983a64e00
wenqiang.xu <wenqiang.xu@okg.com>
2025-08-25 14:08:04 +0800

    dev
    Signed-off-by: wenqiang.xu <wenqiang.xu@okg.com>

--- src/common/network-server/src/tcp/server.rs
+++ src/common/network-server/src/tcp/server.rs
@@ -12,32 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use common_base::error::ResultCommonError;
-// Copyright 2023 RobustMQ Team
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-use common_config::broker::broker_config;
-use grpc_clients::pool::ClientPool;
-use metadata_struct::connection::NetworkConnectionType;
-use observability::mqtt::server::record_broker_thread_num;
-use protocol::codec::RobustMQCodec;
-use std::sync::Arc;
-use std::time::Duration;
-use tokio::net::TcpListener;
-use tokio::sync::broadcast;
-use tokio::time::sleep;
-use tracing::{error, info};
-
 use crate::{
     command::ArcCommandAdapter,
     common::{
@@ -49,6 +23,18 @@ use crate::{
         tls_acceptor::acceptor_tls_process,
     },
 };
+use common_base::error::ResultCommonError;
+use common_config::broker::broker_config;
+use grpc_clients::pool::ClientPool;
+use metadata_struct::connection::NetworkConnectionType;
+use observability::mqtt::server::record_broker_thread_num;
+use protocol::codec::RobustMQCodec;
+use std::sync::Arc;
+use std::time::Duration;
+use tokio::net::TcpListener;
+use tokio::sync::broadcast;
+use tokio::time::sleep;
+use tracing::{error, info};
 
 #[derive(Debug, Clone, Copy)]
 pub struct ProcessorConfig {
