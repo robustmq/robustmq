10693fb26bb034e86fa8e4409ec81f243f435bf5
wenqiang.xu <wenqiang.xu@okg.com>
2025-09-03 20:07:41 +0800

    de v
    Signed-off-by: wenqiang.xu <wenqiang.xu@okg.com>

--- src/common/base/src/tools.rs
+++ src/common/base/src/tools.rs
@@ -189,6 +189,29 @@ where
         .map_err(|e| Status::cancelled(CommonError::CommonError(e.to_string()).to_string()))
 }
 
+pub async fn loop_select<F, Fut>(ac_fn: F, tick_secs: u64, stop_sx: &broadcast::Sender<bool>)
+where
+    F: FnOnce() -> Fut + Copy,
+    Fut: Future<Output = ResultCommonError>,
+{
+    let mut stop_recv = stop_sx.subscribe();
+    let mut internal = tokio::time::interval(Duration::from_secs(tick_secs));
+    loop {
+        select! {
+            val = stop_recv.recv() => {
+                if let Ok(flag) = val {
+                    if flag {
+                        break;
+                    }
+                }
+            }
+            _ = internal.tick() => {
+                let _ = ac_fn().await;
+            }
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use std::net::SocketAddr;
@@ -217,28 +240,5 @@ mod tests {
     fn test_get_addr_by_local_hostname() {
         let ip = get_addr_by_local_hostname(1883);
         let _: SocketAddr = ip.parse().unwrap();
-    }
-}
-
-pub async fn loop_select<F, Fut>(ac_fn: F, tick_secs: u64, stop_sx: &broadcast::Sender<bool>)
-where
-    F: FnOnce() -> Fut + Copy,
-    Fut: Future<Output = ResultCommonError>,
-{
-    let mut stop_recv = stop_sx.subscribe();
-    let mut internal = tokio::time::interval(Duration::from_secs(tick_secs));
-    loop {
-        select! {
-            val = stop_recv.recv() => {
-                if let Ok(flag) = val {
-                    if flag {
-                        break;
-                    }
-                }
-            }
-            _ = internal.tick() => {
-                let _ = ac_fn().await;
-            }
-        }
     }
 }
