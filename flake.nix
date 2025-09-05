344aff511cfd2cf0587b85ff8674369266291da8
Patrick Rall <patrickwrall@gmail.com>
2025-09-05 01:53:47 -0400

    feat: flake and dev shell with crane

---
+++ flake.nix
@@ -0,0 +1,81 @@
+{
+  description = "A reproducible development environment for RobustMQ";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    crane.url = "github:ipetkov/crane";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs =
+    { self
+    , nixpkgs
+    , crane
+    , flake-utils
+    , rust-overlay
+    }:
+    flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [ rust-overlay.overlays.default ];
+      };
+
+      craneLib = crane.mkLib pkgs;
+
+      rustToolchain = pkgs.rust-bin.stable."1.89.0".default.override {
+        extensions = [ "rust-src" "rust-analyzer" "rustfmt" ];
+      };
+
+      cargoToml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
+
+      inherit (cargoToml.workspace.package) version;
+
+      nativeBuildInputs = with pkgs; [
+        pkg-config
+        openssl
+        protobuf
+        clang
+        cmake
+        rdkafka
+        zstd
+        gfortran
+        snappy
+      ];
+
+      commonArgs = {
+        pname = "robustmq";
+        inherit version;
+        src = craneLib.cleanCargoSource ./.;
+        strictDeps = true;
+        inherit nativeBuildInputs;
+      };
+
+
+      cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+
+      robustmq-pkg = craneLib.buildPackage (commonArgs // {
+        inherit cargoArtifacts;
+      });
+    in
+    {
+      packages.default = robustmq-pkg;
+
+      devShells.default = pkgs.mkShell {
+        name = "robustmq-dev-shell";
+
+        inputsFrom = [ cargoArtifacts ];
+        inherit nativeBuildInputs;
+
+        packages = [ rustToolchain ];
+
+        LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
+
+        shellHook = ''
+          echo "Entered RobustMQ development shell. 🚀"
+        '';
+      };
+    }
+    );
+}
