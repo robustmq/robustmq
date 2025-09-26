895484d6f7d4e81f711e2f9e287dfe349784e2e6
wenqiang.xu <wenqiang.xu@okg.com>
2025-09-26 15:01:07 +0800

    dev
    Signed-off-by: wenqiang.xu <wenqiang.xu@okg.com>

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -2,5 +2,11 @@
 import DefaultTheme from 'vitepress/theme'
 import './custom.css'
 import './layout-fix.css'
+import BadgeSection from './components/BadgeSection.vue'
 
-export default DefaultTheme
+export default {
+  extends: DefaultTheme,
+  enhanceApp({ app }) {
+    app.component('BadgeSection', BadgeSection)
+  }
+}
