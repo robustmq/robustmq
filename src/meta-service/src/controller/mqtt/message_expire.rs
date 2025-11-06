b7f7439451045ee9f095cb125672df52eb0eaa61
socutes <socutes@gmail.com>
2025-11-06 12:25:18 +0800

    get_expire_session_list_test
    Signed-off-by: socutes <socutes@gmail.com>

--- src/meta-service/src/controller/mqtt/message_expire.rs
+++ src/meta-service/src/controller/mqtt/message_expire.rs
@@ -76,8 +76,8 @@ impl MessageExpire {
             }
 
             let result_value = value.unwrap().to_vec();
-            let data = serialize::deserialize::<StorageDataWrap<Vec<u8>>>(&result_value).unwrap();
-            let value = serialize::deserialize::<MQTTRetainMessage>(&data.data).unwrap();
+            let data = serialize::deserialize::<StorageDataWrap<MQTTRetainMessage>>(&result_value).unwrap();
+            let value = data.data;
             let delete = now_second() >= (value.create_time + value.retain_message_expired_at);
             if delete {
                 if let Err(e) =
@@ -130,8 +130,8 @@ impl MessageExpire {
             }
 
             let result_value = value.unwrap().to_vec();
-            let data = serialize::deserialize::<StorageDataWrap<Vec<u8>>>(&result_value).unwrap();
-            let value = serialize::deserialize::<MqttLastWillData>(&data.data).unwrap();
+            let data = serialize::deserialize::<StorageDataWrap<MqttLastWillData>>(&result_value).unwrap();
+            let value = data.data;
             if let Some(properties) = value.last_will_properties {
                 let delete = if let Some(expiry_interval) = properties.message_expiry_interval {
                     now_second() >= ((expiry_interval as u64) + data.create_time)
