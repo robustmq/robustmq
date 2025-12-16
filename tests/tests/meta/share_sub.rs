9c734ded992a51cb5c8c2a6f4e38fe76e63a4194
socutes <socutes@gmail.com>
2025-12-16 09:57:15 +0800

    dev
    Signed-off-by: socutes <socutes@gmail.com>

--- tests/tests/meta/share_sub.rs
+++ tests/tests/meta/share_sub.rs
@@ -63,41 +63,4 @@ mod tests {
         assert_eq!(resp.broker_id, node_id);
     }
 
-    #[tokio::test]
-    async fn test_queue_sub() {
-        let client_pool = Arc::new(ClientPool::new(3));
-        let addrs = vec!["127.0.0.1:1228".to_string()];
-
-        let node_ip = "127.0.0.1".to_string();
-        let node_id = 7;
-        let node_inner_addr = "127.0.0.1:8228".to_string();
-        let extend_info = Vec::new();
-        let node = BrokerNode {
-            roles: Vec::new(),
-            node_ip,
-            node_id,
-            node_inner_addr,
-            extend: extend_info.clone(),
-            register_time: now_second(),
-            start_time: now_second(),
-        };
-        let request = RegisterNodeRequest {
-            node: node.encode().unwrap(),
-        };
-
-        sleep(Duration::from_secs(2));
-
-        let res = register_node(&client_pool, &addrs, request).await.unwrap();
-        info!("{:?}", res);
-
-        let group_name = "$queue/topic1".to_string();
-        let req = GetShareSubLeaderRequest { group_name };
-
-        let resp = placement_get_share_sub_leader(&client_pool, &addrs, req)
-            .await
-            .unwrap();
-        println!("resp broker_id:{}", resp.broker_id);
-        println!("node_id:{node_id}");
-        assert_eq!(resp.broker_id, node_id);
-    }
 }
