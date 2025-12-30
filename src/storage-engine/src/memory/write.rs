680a458ec19190e18123b27cae64e4107cbb0178
socutes <socutes@gmail.com>
2025-12-30 18:33:33 +0800

    dev
    Signed-off-by: socutes <socutes@gmail.com>

--- src/storage-engine/src/memory/write.rs
+++ src/storage-engine/src/memory/write.rs
@@ -109,12 +109,42 @@ impl MemoryStorageEngine {
         if next_num > self.config.max_records_per_shard {
             let offset = self.get_earliest_offset(shard_name)?;
             let discard_num = (current_shard_data.len() as f64 * 0.2) as u64;
-            for i in offset..(offset + discard_num) {
-                current_shard_data.remove(&i);
+
+            if let Some(key_map) = self.key_index.get_mut(shard_name) {
+                for i in offset..(offset + discard_num) {
+                    if let Some((_, record)) = current_shard_data.remove(&i) {
+                        if let Some(key) = &record.metadata.key {
+                            key_map.remove(key);
+                        }
+                    }
+                }
+            } else {
+                for i in offset..(offset + discard_num) {
+                    current_shard_data.remove(&i);
+                }
             }
-            self.save_earliest_offset(shard_name, offset + discard_num)?;
-            // todo remove old index
+
+            let new_earliest = offset + discard_num;
+            self.save_earliest_offset(shard_name, new_earliest)?;
+            self.cleanup_indexes_by_offset(shard_name, new_earliest);
         }
         Ok(())
+    }
+
+    fn cleanup_indexes_by_offset(&self, shard_name: &str, earliest_offset: u64) {
+        if let Some(tag_map) = self.tag_index.get_mut(shard_name) {
+            for mut offsets in tag_map.iter_mut() {
+                offsets.value_mut().retain(|&o| o >= earliest_offset);
+            }
+            tag_map.retain(|_, v| !v.is_empty());
+        }
+
+        if let Some(key_map) = self.key_index.get_mut(shard_name) {
+            key_map.retain(|_, &mut o| o >= earliest_offset);
+        }
+
+        if let Some(ts_map) = self.timestamp_index.get_mut(shard_name) {
+            ts_map.retain(|_, &mut o| o >= earliest_offset);
+        }
     }
 }
