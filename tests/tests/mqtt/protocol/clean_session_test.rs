43a219bf2b52d8292bf11ba61a38d2214065497d
socutes <socutes@gmail.com>
2026-01-24 14:33:10 +0800

    dev
    Signed-off-by: socutes <socutes@gmail.com>

--- tests/tests/mqtt/protocol/clean_session_test.rs
+++ tests/tests/mqtt/protocol/clean_session_test.rs
@@ -23,45 +23,81 @@ mod tests {
     };
     use common_base::tools::unique_id;
     use paho_mqtt::{
-        Client, ConnectOptionsBuilder, CreateOptionsBuilder, Properties, PropertyCode, ReasonCode,
+        Client, ConnectOptionsBuilder, CreateOptionsBuilder, DisconnectOptionsBuilder, Properties,
+        PropertyCode, ReasonCode,
     };
     use std::time::Duration;
     use tokio::time::sleep;
 
+    async fn session_count_by_admin(client_id: &str) -> usize {
+        let admin_client = create_test_env().await;
+        let request = SessionListReq {
+            client_id: Some(client_id.to_string()),
+            ..Default::default()
+        };
+        let data: PageReplyData<Vec<SessionListRow>> =
+            admin_client.get_session_list(&request).await.unwrap();
+        data.total_count
+    }
+
+    fn new_client(addr: &str, client_id: &str) -> Client {
+        let create_opts = CreateOptionsBuilder::new()
+            .server_uri(addr)
+            .client_id(client_id)
+            .finalize();
+        Client::new(create_opts).unwrap()
+    }
+
+    fn connect_mqtt34(addr: &str, client_id: &str, clean_session: bool) -> (Client, bool) {
+        let cli = new_client(addr, client_id);
+        let mut conn_opts = ConnectOptionsBuilder::with_mqtt_version(3);
+        conn_opts
+            .user_name(username())
+            .password(password())
+            .clean_session(clean_session);
+        let result = cli.connect(conn_opts.finalize()).unwrap();
+        assert_eq!(result.reason_code(), ReasonCode::Success);
+        let session_present = result.connect_response().unwrap().session_present;
+        (cli, session_present)
+    }
+
+    fn connect_mqtt5(
+        addr: &str,
+        client_id: &str,
+        clean_start: bool,
+        expiry: u32,
+    ) -> (Client, bool) {
+        let cli = new_client(addr, client_id);
+        let mut conn_opts = ConnectOptionsBuilder::new_v5();
+
+        let mut props = Properties::new();
+        props
+            .push_u32(PropertyCode::SessionExpiryInterval, expiry)
+            .unwrap();
+
+        conn_opts
+            .user_name(username())
+            .password(password())
+            .properties(props)
+            .clean_start(clean_start);
+
+        let result = cli.connect(conn_opts.finalize()).unwrap();
+        assert_eq!(result.reason_code(), ReasonCode::Success);
+        let session_present = result.connect_response().unwrap().session_present;
+        (cli, session_present)
+    }
+
+    // mqtt 3/4. clean_session true/false
     #[tokio::test]
     async fn mqtt34_clean_session_true_test() {
         let addr = broker_addr_by_type("tcp");
         let client_id = test_client_id();
-        for i in 0..3 {
-            let create_opts = CreateOptionsBuilder::new()
-                .server_uri(addr.clone())
-                .client_id(client_id.clone())
-                .finalize();
-
-            let cli = Client::new(create_opts).unwrap();
-            let mut conn_opts = ConnectOptionsBuilder::with_mqtt_version(3);
-            conn_opts
-                .user_name(username())
-                .password(password())
-                .clean_session(true);
-            let conn_opts = conn_opts.finalize();
-
-            let result = cli.connect(conn_opts).unwrap();
-            assert_eq!(result.reason_code(), ReasonCode::Success);
-            let conn_resp = result.connect_response().unwrap();
-            assert!(!conn_resp.session_present);
+        for _ in 0..3 {
+            let (cli, session_present) = connect_mqtt34(&addr, &client_id, true);
+            assert!(!session_present);
             distinct_conn(cli);
             sleep(Duration::from_secs(1)).await;
-
-            let admin_client = create_test_env().await;
-            let request = SessionListReq {
-                client_id: Some(client_id.clone()),
-                ..Default::default()
-            };
-            let data: PageReplyData<Vec<SessionListRow>> =
-                admin_client.get_session_list(&request).await.unwrap();
-            println!("{},{},{:?}", i, client_id, data);
-            assert_eq!(data.total_count, 0);
+            assert_eq!(session_count_by_admin(&client_id).await, 0);
         }
     }
 
@@ -70,92 +106,33 @@ mod tests {
         let addr = broker_addr_by_type("tcp");
         let client_id = test_client_id();
         for i in 0..3 {
-            let create_opts = CreateOptionsBuilder::new()
-                .server_uri(addr.clone())
-                .client_id(client_id.clone())
-                .finalize();
-
-            let cli = Client::new(create_opts).unwrap();
-            let mut conn_opts = ConnectOptionsBuilder::with_mqtt_version(3);
-            conn_opts
-                .user_name(username())
-                .password(password())
-                .clean_session(false);
-            let conn_opts = conn_opts.finalize();
-
-            let result = cli.connect(conn_opts).unwrap();
-            assert_eq!(result.reason_code(), ReasonCode::Success);
+            let (cli, session_present) = connect_mqtt34(&addr, &client_id, false);
             if i == 0 {
-                let conn_resp = result.connect_response().unwrap();
-                assert!(!conn_resp.session_present);
+                assert!(!session_present);
                 distinct_conn(cli);
                 sleep(Duration::from_secs(1)).await;
-
-                let admin_client = create_test_env().await;
-                let request = SessionListReq {
-                    client_id: Some(client_id.clone()),
-                    ..Default::default()
-                };
-                let data: PageReplyData<Vec<SessionListRow>> =
-                    admin_client.get_session_list(&request).await.unwrap();
-                assert_eq!(data.total_count, 1);
+                assert_eq!(session_count_by_admin(&client_id).await, 1);
             } else {
-                let conn_resp = result.connect_response().unwrap();
-                assert!(conn_resp.session_present);
+                assert!(session_present);
                 distinct_conn(cli);
                 sleep(Duration::from_secs(1)).await;
-
-                let admin_client = create_test_env().await;
-
-                let request = SessionListReq {
-                    client_id: Some(client_id.clone()),
-                    ..Default::default()
-                };
-                let data: PageReplyData<Vec<SessionListRow>> =
-                    admin_client.get_session_list(&request).await.unwrap();
-                assert_eq!(data.total_count, 1);
+                assert_eq!(session_count_by_admin(&client_id).await, 1);
             }
         }
     }
 
+    // mqtt 5 clean_session true/false. expiry 0/30
     #[tokio::test]
     async fn mqtt5_clean_session_true_expiry_0_test() {
         let addr = broker_addr_by_type("tcp");
         let client_id = test_client_id();
         for _ in 0..3 {
-            let create_opts = CreateOptionsBuilder::new()
-                .server_uri(addr.clone())
-                .client_id(client_id.clone())
-                .finalize();
-
-            let cli = Client::new(create_opts).unwrap();
-            let mut conn_opts = ConnectOptionsBuilder::new_v5();
-            let mut props = Properties::new();
-            props
-                .push_u32(PropertyCode::SessionExpiryInterval, 0)
-                .unwrap();
-            conn_opts
-                .user_name(username())
-                .password(password())
-                .properties(props)
-                .clean_start(true);
-            let conn_opts = conn_opts.finalize();
-
-            let result = cli.connect(conn_opts).unwrap();
-            assert_eq!(result.reason_code(), ReasonCode::Success);
-            let conn_resp = result.connect_response().unwrap();
-            assert!(!conn_resp.session_present);
+            let (cli, session_present) = connect_mqtt5(&addr, &client_id, true, 0);
+            assert!(!session_present);
             distinct_conn(cli);
 
             sleep(Duration::from_secs(1)).await;
-            let admin_client = create_test_env().await;
-            let request = SessionListReq {
-                client_id: Some(client_id.clone()),
-                ..Default::default()
-            };
-            let data: PageReplyData<Vec<SessionListRow>> =
-                admin_client.get_session_list(&request).await.unwrap();
-            assert_eq!(data.total_count, 0);
+            assert_eq!(session_count_by_admin(&client_id).await, 0);
         }
     }
 
@@ -164,39 +141,12 @@ mod tests {
         let addr = broker_addr_by_type("tcp");
         let client_id = unique_id();
         for _ in 0..3 {
-            let create_opts = CreateOptionsBuilder::new()
-                .server_uri(addr.clone())
-                .client_id(client_id.clone())
-                .finalize();
-
-            let cli = Client::new(create_opts).unwrap();
-            let mut conn_opts = ConnectOptionsBuilder::new_v5();
-            let mut props = Properties::new();
-            props
-                .push_u32(PropertyCode::SessionExpiryInterval, 30)
-                .unwrap();
-            conn_opts
-                .user_name(username())
-                .password(password())
-                .properties(props)
-                .clean_start(true);
-            let conn_opts = conn_opts.finalize();
-
-            let result = cli.connect(conn_opts).unwrap();
-            assert_eq!(result.reason_code(), ReasonCode::Success);
-            let conn_resp = result.connect_response().unwrap();
-            assert!(!conn_resp.session_present);
+            let (cli, session_present) = connect_mqtt5(&addr, &client_id, true, 30);
+            assert!(!session_present);
             distinct_conn(cli);
 
             sleep(Duration::from_secs(1)).await;
-            let admin_client = create_test_env().await;
-            let request = SessionListReq {
-                client_id: Some(client_id.clone()),
-                ..Default::default()
-            };
-            let data: PageReplyData<Vec<SessionListRow>> =
-                admin_client.get_session_list(&request).await.unwrap();
-            assert_eq!(data.total_count, 1);
+            assert_eq!(session_count_by_admin(&client_id).await, 1);
         }
     }
 
@@ -205,95 +155,57 @@ mod tests {
         let addr = broker_addr_by_type("tcp");
         let client_id = unique_id();
         for _ in 0..3 {
-            let create_opts = CreateOptionsBuilder::new()
-                .server_uri(addr.clone())
-                .client_id(client_id.clone())
-                .finalize();
-
-            let cli = Client::new(create_opts).unwrap();
-            let mut conn_opts = ConnectOptionsBuilder::new_v5();
-            let mut props = Properties::new();
-            props
-                .push_u32(PropertyCode::SessionExpiryInterval, 0)
-                .unwrap();
-            conn_opts
-                .user_name(username())
-                .password(password())
-                .properties(props)
-                .clean_start(false);
-            let conn_opts = conn_opts.finalize();
-
-            let result = cli.connect(conn_opts).unwrap();
-            assert_eq!(result.reason_code(), ReasonCode::Success);
-            let conn_resp = result.connect_response().unwrap();
-            assert!(!conn_resp.session_present);
+            let (cli, session_present) = connect_mqtt5(&addr, &client_id, false, 0);
+            assert!(!session_present);
             distinct_conn(cli);
 
             sleep(Duration::from_secs(1)).await;
-            let admin_client = create_test_env().await;
-            let request = SessionListReq {
-                client_id: Some(client_id.clone()),
-                ..Default::default()
-            };
-            let data: PageReplyData<Vec<SessionListRow>> =
-                admin_client.get_session_list(&request).await.unwrap();
-            assert_eq!(data.total_count, 0);
+            assert_eq!(session_count_by_admin(&client_id).await, 0);
         }
     }
 
     #[tokio::test]
-    async fn mqtt5_clean_session_false_expiry_30_test() {
+    async fn mqtt5_clean_distinct_expiry_60() {
         let addr = broker_addr_by_type("tcp");
         let client_id = unique_id();
-        for i in 0..3 {
-            let create_opts = CreateOptionsBuilder::new()
-                .server_uri(addr.clone())
-                .client_id(client_id.clone())
-                .finalize();
 
-            let cli = Client::new(create_opts).unwrap();
-            let mut conn_opts = ConnectOptionsBuilder::new_v5();
-            let mut props = Properties::new();
-            props
-                .push_u32(PropertyCode::SessionExpiryInterval, 30)
-                .unwrap();
-            conn_opts
-                .user_name(username())
-                .password(password())
-                .properties(props)
-                .clean_start(false);
-            let conn_opts = conn_opts.finalize();
+        let (cli, session_present) = connect_mqtt5(&addr, &client_id, false, 30);
+        assert!(!session_present);
 
-            let result = cli.connect(conn_opts).unwrap();
-            assert_eq!(result.reason_code(), ReasonCode::Success);
-            let conn_resp = result.connect_response().unwrap();
-            if i == 0 {
-                assert!(!conn_resp.session_present);
-                distinct_conn(cli);
+        // session expire = 30
+        let admin_client = create_test_env().await;
+        let request = SessionListReq {
+            client_id: Some(client_id.to_string()),
+            ..Default::default()
+        };
+        let data: PageReplyData<Vec<SessionListRow>> =
+            admin_client.get_session_list(&request).await.unwrap();
+        let raw = data.data.first().unwrap();
+        assert_eq!(raw.session_expiry, 30);
 
-                sleep(Duration::from_secs(1)).await;
-                let admin_client = create_test_env().await;
-                let request = SessionListReq {
-                    client_id: Some(client_id.clone()),
-                    ..Default::default()
-                };
-                let data: PageReplyData<Vec<SessionListRow>> =
-                    admin_client.get_session_list(&request).await.unwrap();
-                assert_eq!(data.total_count, 1);
-            } else {
-                assert!(conn_resp.session_present);
-                distinct_conn(cli);
+        // distinct connection
+        let mut props = Properties::new();
+        props
+            .push_u32(PropertyCode::SessionExpiryInterval, 60)
+            .unwrap();
+        let disconnect_opts = DisconnectOptionsBuilder::new()
+            .reason_code(ReasonCode::DisconnectWithWillMessage)
+            .properties(props)
+            .finalize();
+        let res = cli.disconnect(disconnect_opts);
+        assert!(res.is_ok());
 
-                sleep(Duration::from_secs(1)).await;
-                let admin_client = create_test_env().await;
-                let request = SessionListReq {
-                    client_id: Some(client_id.clone()),
-                    ..Default::default()
-                };
-                let data: PageReplyData<Vec<SessionListRow>> =
-                    admin_client.get_session_list(&request).await.unwrap();
-                assert_eq!(data.total_count, 1);
-            }
-        }
+        sleep(Duration::from_secs(1)).await;
+
+        // session expire = 30
+        let request = SessionListReq {
+            client_id: Some(client_id.to_string()),
+            ..Default::default()
+        };
+        let data: PageReplyData<Vec<SessionListRow>> =
+            admin_client.get_session_list(&request).await.unwrap();
+        assert_eq!(session_count_by_admin(&client_id).await, 1);
+        let raw = data.data.first().unwrap();
+        assert_eq!(raw.session_expiry, 60);
     }
 }
