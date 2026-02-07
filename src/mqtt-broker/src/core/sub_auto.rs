f1a7f5bc655a3aed6c3dfcee664603e692bf9b74
socutes <socutes@gmail.com>
2026-02-07 21:39:28 +0800

    dev
    Signed-off-by: socutes <socutes@gmail.com>

--- src/mqtt-broker/src/core/sub_auto.rs
+++ src/mqtt-broker/src/core/sub_auto.rs
@@ -19,63 +19,90 @@ use super::{
 use crate::core::tool::ResultMqttBrokerError;
 use crate::subscribe::manager::SubscribeManager;
 use grpc_clients::pool::ClientPool;
-use metadata_struct::mqtt::auto_subscribe_rule::MqttAutoSubscribeRule;
 use protocol::mqtt::common::{Filter, Login, MqttProtocol, Subscribe};
 use std::sync::Arc;
+use tracing::{debug, error, info};
+
+fn replace_topic_placeholders(
+    pattern: &str,
+    client_id: &str,
+    username: &str,
+    remote_addr: &str,
+) -> String {
+    pattern
+        .replace("${clientid}", client_id)
+        .replace("${username}", username)
+        .replace("${host}", remote_addr)
+}
 
 pub async fn try_auto_subscribe(
     client_id: String,
     login: &Option<Login>,
+    remote_addr: String,
     protocol: &MqttProtocol,
     client_pool: &Arc<ClientPool>,
     cache_manager: &Arc<MQTTCacheManager>,
     subscribe_manager: &Arc<SubscribeManager>,
 ) -> ResultMqttBrokerError {
-    let auto_subscribe_rules: Vec<MqttAutoSubscribeRule> = cache_manager
-        .auto_subscribe_rule
-        .iter()
-        .map(|entry| entry.value().clone())
-        .collect();
+    if cache_manager.auto_subscribe_rule.is_empty() {
+        return Ok(());
+    }
 
-    let username: String = if let Some(login_info) = login {
-        login_info.username.clone()
-    } else {
-        "".to_string()
-    };
+    let username = login
+        .as_ref()
+        .map(|login_info| login_info.username.as_str())
+        .unwrap_or("");
 
     let mut filters: Vec<Filter> = Vec::new();
-    for auto_subscribe_rule in auto_subscribe_rules {
-        let mut path: String = auto_subscribe_rule.topic.clone();
-        path = path.replace("${clientid}", &client_id);
-        if !username.is_empty() {
-            path = path.replace("${username}", &username);
-        }
+
+    for rule_entry in cache_manager.auto_subscribe_rule.iter() {
+        let rule = rule_entry.value();
+        let topic = replace_topic_placeholders(&rule.topic, &client_id, username, &remote_addr);
+
+        debug!(
+            "Auto-subscribe: client_id={}, original_pattern={}, resolved_topic={}, qos={:?}",
+            client_id, rule.topic, topic, rule.qos
+        );
 
         filters.push(Filter {
-            path,
-            qos: auto_subscribe_rule.qos,
-            no_local: auto_subscribe_rule.no_local,
-            preserve_retain: auto_subscribe_rule.retain_as_published,
-            retain_handling: auto_subscribe_rule.retained_handling,
+            path: topic,
+            qos: rule.qos,
+            no_local: rule.no_local,
+            preserve_retain: rule.retain_as_published,
+            retain_handling: rule.retained_handling.clone(),
         });
     }
 
     if !filters.is_empty() {
-        let subscribe: Subscribe = Subscribe {
+        info!(
+            "Applying {} auto-subscription rule(s) for client: {}",
+            filters.len(),
+            client_id
+        );
+
+        let subscribe = Subscribe {
             packet_identifier: 0,
-            filters: filters.clone(),
+            filters,
         };
 
-        save_subscribe(SaveSubscribeContext {
+        if let Err(e) = save_subscribe(SaveSubscribeContext {
             client_id: client_id.clone(),
             protocol: protocol.clone(),
             client_pool: client_pool.clone(),
             cache_manager: cache_manager.clone(),
             subscribe_manager: subscribe_manager.clone(),
-            subscribe: subscribe.clone(),
+            subscribe,
             subscribe_properties: None,
         })
-        .await?;
+        .await
+        {
+            error!(
+                "Failed to apply auto-subscription for client {}: {:?}",
+                client_id, e
+            );
+            return Err(e);
+        }
     }
+
     Ok(())
 }
