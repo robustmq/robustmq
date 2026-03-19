0a1a6bd024e9b064ddbae7c239ab3536f2058874
socutes <socutes@gmail.com>
2026-03-19 14:32:43 +0800

    dev
    Signed-off-by: socutes <socutes@gmail.com>

--- src/common/rate-limit/src/global.rs
+++ src/common/rate-limit/src/global.rs
@@ -12,47 +12,79 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use common_base::error::ResultCommonError;
+use crate::{ArcLockRateLimiter, ArcRateLimiter};
+use common_base::error::{common::CommonError, ResultCommonError};
+use common_config::broker::broker_config;
 use dashmap::DashMap;
 use governor::{Quota, RateLimiter};
 use std::{num::NonZero, sync::Arc};
-
-use crate::ArcRateLimiter;
+use tokio::sync::RwLock;
 
 #[derive(Clone)]
 pub struct GlobalRateLimiterManager {
+    // (uri, ArcRateLimiter)
     http_limits: DashMap<String, ArcRateLimiter>,
-    network_connection_limit: ArcRateLimiter,
-}
-impl Default for GlobalRateLimiterManager {
-    fn default() -> Self {
-        Self::new()
-    }
+
+    // ArcRateLimiter
+    network_connection_rate: ArcLockRateLimiter,
 }
 
 impl GlobalRateLimiterManager {
-    pub fn new() -> Self {
-        let non_zero = NonZero::new(42).unwrap();
-        GlobalRateLimiterManager {
-            http_limits: DashMap::with_capacity(2),
-            network_connection_limit: Arc::new(RateLimiter::direct(Quota::per_second(non_zero))),
-        }
+    pub fn new(connection_rate: u32) -> Result<Self, Box<CommonError>> {
+        let connection_non_zero = NonZero::new(connection_rate).ok_or_else(|| {
+            Box::new(CommonError::InvalidRateLimitValue(
+                "connection_rate".to_string(),
+            ))
+        })?;
+
+        Ok(GlobalRateLimiterManager {
+            http_limits: DashMap::with_capacity(8),
+            network_connection_rate: Arc::new(RwLock::new(RateLimiter::direct(Quota::per_second(
+                connection_non_zero,
+            )))),
+        })
     }
 
-    pub async fn wait_http_limit(&self, uri: String) -> ResultCommonError {
+    pub fn set_http_rate(&self, uri: &str, rate: u32) -> Result<ArcRateLimiter, Box<CommonError>> {
+        let non_zero = NonZero::new(rate)
+            .ok_or_else(|| Box::new(CommonError::InvalidRateLimitValue("http_rate".to_string())))?;
+        let limit = Arc::new(RateLimiter::direct(Quota::per_second(non_zero)));
+        self.http_limits.insert(uri.to_string(), limit.clone());
+        Ok(limit)
+    }
+
+    pub async fn set_network_connection_rate(&self, rate: u32) -> ResultCommonError {
+        let mut write = self.network_connection_rate.write().await;
+        let non_zero = NonZero::new(rate).ok_or_else(|| {
+            CommonError::InvalidRateLimitValue("network_connection_rate".to_string())
+        })?;
+        *write = RateLimiter::direct(Quota::per_second(non_zero));
+        Ok(())
+    }
+
+    pub async fn http_uri_rate_limit(&self, uri: String) -> Result<(), Box<CommonError>> {
         if let Some(limit) = self.http_limits.get(&uri) {
             limit.until_ready().await;
         } else {
-            let non_zero = NonZero::new(42).unwrap();
-            let limit = Arc::new(RateLimiter::direct(Quota::per_second(non_zero)));
+            let config = broker_config();
+            let connection_non_zero = NonZero::new(
+                config.cluster_limit.max_network_connection_rate,
+            )
+            .ok_or_else(|| {
+                Box::new(CommonError::InvalidRateLimitValue(
+                    "network_connection_rate".to_string(),
+                ))
+            })?;
+            let limit = Arc::new(RateLimiter::direct(Quota::per_second(connection_non_zero)));
             limit.until_ready().await;
             self.http_limits.insert(uri, limit);
         }
         Ok(())
     }
 
-    pub async fn wait_network_connection_limit(&self) -> ResultCommonError {
-        self.network_connection_limit.until_ready().await;
+    pub async fn network_connection_rate_limit(&self) -> ResultCommonError {
+        let read = self.network_connection_rate.read().await;
+        read.until_ready().await;
         Ok(())
     }
 }
